e81438ac21d85dd62f6e5e66516c37acb06d9bf0
Igie-oss <baldesansoigieboysanchez@gmail.com>
2024-03-13 21:40:46 +0800

    Setup redux state

---
+++ front_end/src/service/store.ts
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useSelector } from "react-redux/es/exports";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { useDispatch } from "react-redux";
+import { apiSlice } from "./slices/api/apiSlice";
+import userReducer from "./slices/user/userSlice";
+const store = configureStore({
+	reducer: {
+		[apiSlice.reducerPath]: apiSlice.reducer,
+		user: userReducer,
+	},
+	middleware: (getDefaultMiddleware) => [
+		...getDefaultMiddleware(),
+		apiSlice.middleware,
+	],
+	devTools: false,
+});
+setupListeners(store.dispatch);
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
