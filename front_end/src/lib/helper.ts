7e04a0e4927df66e612176ebfdde053454fdd5f0
Igie-oss <baldesansoigieboysanchez@gmail.com>
2024-03-14 15:50:25 +0800

    helper

---
+++ front_end/src/lib/helper.ts
@@ -0,0 +1,21 @@
+import CryptoJS from "crypto-js";
+
+export const decryptText = (text: string): string => {
+	const decData = CryptoJS.enc.Base64.parse(text).toString(CryptoJS.enc.Utf8);
+	const bytes = CryptoJS.AES.decrypt(
+		decData,
+		`${import.meta.env.VITE_SECRET_KEY}`
+	).toString(CryptoJS.enc.Utf8);
+	return JSON.parse(bytes);
+};
+
+export const encryptText = (text: string): string => {
+	const encJson = CryptoJS.AES.encrypt(
+		JSON.stringify(text),
+		`${import.meta.env.VITE_SECRET_KEY}`
+	).toString();
+	const encData = CryptoJS.enc.Base64.stringify(
+		CryptoJS.enc.Utf8.parse(encJson)
+	);
+	return encData;
+};
