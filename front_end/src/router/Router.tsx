f31661a8c65203ac9cca763d1135746394b6d360
Igie-oss <baldesansoigieboysanchez@gmail.com>
2024-03-14 15:50:50 +0800

    Update router add route guard

--- front_end/src/router/Router.tsx
+++ front_end/src/router/Router.tsx
@@ -1,29 +1,46 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Persist from "@/pages/authPage/login/Persist";
+import RouteGuard from "./RouteGuard";
+import LoaderSpinner from "@/components/loader/LoaderSpinner";
 export default function Router() {
 	const LandingPage = lazy(() => import("@/pages/landingPage/LandingPage"));
+
+	const LoginPage = lazy(() => import("@/pages/authPage/login/LoginPage"));
+
 	const RegisterPage = lazy(
 		() => import("@/pages/authPage/register/RegisterPage")
 	);
-	const LoginPage = lazy(() => import("@/pages/authPage/login/LoginPage"));
+
+	const RegisterForm = lazy(
+		() => import("@/pages/authPage/register/RegisterForm")
+	);
+
+	const RegisterVerifyOtp = lazy(
+		() => import("@/pages/authPage/register/RegisterVeryOtp")
+	);
+
+	const MainPage = lazy(() => import("@/pages/mainPage/MainPage"));
 	return (
-		<Suspense fallback={<p>loading...</p>}>
+		<Suspense fallback={<LoaderSpinner />}>
 			<BrowserRouter>
 				<Routes>
 					<Route path="*" element={<p>Error</p>} />
 					{/* <Route element={<Redirect />}> */}
 					<Route path="/" element={<LandingPage />} />
 					<Route path="/login" element={<LoginPage />} />
-					<Route path="/login" element={<RegisterPage />} />
+					<Route path="/register" element={<RegisterPage />}>
+						<Route path="/register/form" element={<RegisterForm />} />
+						<Route path="/register/otp" element={<RegisterVerifyOtp />} />
+					</Route>
 					{/* </Route> */}
-					{/* <Route path="/register" element={<RegisterPage />} />
 					<Route element={<Persist />}>
-						<Route element={<ProtectedRoutes />}>
-							<Route path="/message" element={<MainPage />}>
-								<Route path="/message/channel" element={<ChatScreen />} />
+						<Route element={<RouteGuard />}>
+							<Route path="/chat" element={<MainPage />}>
+								{/* <Route path="/message/channel" element={<ChatScreen />} /> */}
 							</Route>
 						</Route>
-					</Route> */}
+					</Route>
 				</Routes>
 			</BrowserRouter>
 		</Suspense>
