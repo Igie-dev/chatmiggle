0058361684ac939048cdd76d7b01301e0e09dd45
Igie-oss <baldesansoigieboysanchez@gmail.com>
2024-03-16 17:50:38 +0800

    Group list screen

---
+++ front_end/src/pages/mainPage/screens/groupListScreen/aside/Aside.tsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+export default function AsideNav() {
+	return (
+		<aside className="absolute top-0 left-0 z-50 w-full h-full bg-secondary/60 lg:static lg:w-[25rem]">
+			<div className="flex flex-col h-full w-[22rem] py-5 px-4 bg-background border-r lg:w-full lg:border-none">
+				<Header />
+				Group list
+			</div>
+		</aside>
+	);
+}
