838cee3400e3a5d3eee995b20cb4231436b56413
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-03-28 20:47:15 +0800

    Update channel aside

--- front_end/src/pages/mainPage/screens/channelListScreen/ChannelListScreen.tsx
+++ front_end/src/pages/mainPage/screens/channelListScreen/ChannelListScreen.tsx
@@ -1,13 +1,22 @@
 import { Outlet } from "react-router-dom";
 import AsideNav from "./aside/Aside";
-
+import { useRef } from "react";
 export default function ChannelListScreen() {
-	return (
-		<section className="relative flex w-screen h-screen">
-			<AsideNav />
-			<main className="w-full h-full border lg:flex-1 bg-secondary">
-				<Outlet />
-			</main>
-		</section>
-	);
+  const asideRef = useRef<HTMLElement | null>(null);
+  const handleAside = () => {
+    if (asideRef?.current?.classList.contains("-translate-x-full")) {
+      asideRef?.current?.classList.remove("-translate-x-full");
+    } else {
+      asideRef?.current?.classList.add("-translate-x-full");
+    }
+  };
+
+  return (
+    <section className="relative flex w-screen h-screen">
+      <AsideNav asideRef={asideRef} handleAside={handleAside} />
+      <main className="w-full h-full border lg:flex-1 bg-primary-foreground">
+        <Outlet />
+      </main>
+    </section>
+  );
 }
--- front_end/src/pages/mainPage/screens/channelListScreen/aside/Aside.tsx
+++ front_end/src/pages/mainPage/screens/channelListScreen/aside/Aside.tsx
@@ -1,11 +1,21 @@
-import Header from "../../components/Header";
-export default function AsideNav() {
-	return (
-		<aside className="absolute top-0 left-0 z-50 w-full h-full bg-secondary/60 lg:static lg:w-[25rem]">
-			<div className="flex flex-col h-full w-[22rem] py-5 px-4 bg-background border-r lg:w-full lg:border-none">
-				<Header />
-				Channel list
-			</div>
-		</aside>
-	);
+import { ForwardedRef } from "react";
+import Header from "../../../shared/Header";
+import ChannelList from "./ChannelList";
+
+type Props = {
+  handleAside: () => void;
+  asideRef: ForwardedRef<HTMLElement | null>;
+};
+export default function AsideNav({ handleAside, asideRef }: Props) {
+  return (
+    <aside
+      ref={asideRef}
+      className="absolute top-0 left-0 z-50 w-full h-full bg-primary-foreground/60 transition-all lg:static lg:translate-x-0 lg:w-[22rem]"
+    >
+      <div className="flex flex-col h-full w-[22rem] gap-2 py-5 px-4 bg-background border-r lg:w-full lg:border-none">
+        <Header />
+        <ChannelList handleAside={handleAside} />
+      </div>
+    </aside>
+  );
 }
