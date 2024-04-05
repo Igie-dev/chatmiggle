c2c85c48f6282708e10dc77bfb5aae0edfbb1e4e
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-06 01:02:59 +0800

    Remove users list

--- front_end/src/router/Router.tsx
+++ front_end/src/router/Router.tsx
@@ -5,87 +5,79 @@ import RouteGuard from "./RouteGuard";
 import LoaderSpinner from "@/components/loader/LoaderSpinner";
 import Redirect from "./Redirect";
 export default function Router() {
-	const LandingPage = lazy(() => import("@/pages/landingPage/LandingPage"));
+  const LandingPage = lazy(() => import("@/pages/landingPage/LandingPage"));
 
-	const LoginPage = lazy(() => import("@/pages/authPage/login/LoginPage"));
+  const LoginPage = lazy(() => import("@/pages/authPage/login/LoginPage"));
 
-	const RegisterPage = lazy(
-		() => import("@/pages/authPage/register/RegisterPage")
-	);
+  const RegisterPage = lazy(
+    () => import("@/pages/authPage/register/RegisterPage")
+  );
 
-	const RegisterForm = lazy(
-		() => import("@/pages/authPage/register/RegisterForm")
-	);
+  const RegisterForm = lazy(
+    () => import("@/pages/authPage/register/RegisterForm")
+  );
 
-	const RegisterVerifyOtp = lazy(
-		() => import("@/pages/authPage/register/RegisterVeryOtp")
-	);
+  const RegisterVerifyOtp = lazy(
+    () => import("@/pages/authPage/register/RegisterVeryOtp")
+  );
 
-	const ChatBox = lazy(
-		() => import("@/pages/mainPage/screens/chatBox/ChatBox")
-	);
+  const ChatBox = lazy(
+    () => import("@/pages/mainPage/screens/chatBox/ChatBox")
+  );
 
-	const ChannelListScrren = lazy(
-		() => import("@/pages/mainPage/screens/channelListScreen/ChannelListScreen")
-	);
+  const ChannelListScrren = lazy(
+    () => import("@/pages/mainPage/screens/channelListScreen/ChannelListScreen")
+  );
 
-	const UserListScreen = lazy(
-		() => import("@/pages/mainPage/screens/userListScreen/UserListScreen")
-	);
+  const GroupListScreen = lazy(
+    () => import("@/pages/mainPage/screens/groupListScreen/GroupListScreen")
+  );
 
-	const GroupListScreen = lazy(
-		() => import("@/pages/mainPage/screens/groupListScreen/GroupListScreen")
-	);
+  const AvatarScreen = lazy(
+    () => import("@/pages/mainPage/screens/avatarScreen/AvatarScreen")
+  );
 
-	const AvatarScreen = lazy(
-		() => import("@/pages/mainPage/screens/avatarScreen/AvatarScreen")
-	);
+  const UploadAvatar = lazy(
+    () => import("@/pages/mainPage/screens/avatarScreen/forms/UploadAvatar")
+  );
 
-	const UploadAvatar = lazy(
-		() => import("@/pages/mainPage/screens/avatarScreen/forms/UploadAvatar")
-	);
+  const RemovedAvatar = lazy(
+    () => import("@/pages/mainPage/screens/avatarScreen/forms/RemoveAvatar")
+  );
+  return (
+    <Suspense fallback={<LoaderSpinner />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<p>Error</p>} />
+          {/* <Route element={<Redirect />}> */}
+          <Route element={<Redirect />}>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Route>
 
-	const RemovedAvatar = lazy(
-		() => import("@/pages/mainPage/screens/avatarScreen/forms/RemoveAvatar")
-	);
-	return (
-		<Suspense fallback={<LoaderSpinner />}>
-			<BrowserRouter>
-				<Routes>
-					<Route path="*" element={<p>Error</p>} />
-					{/* <Route element={<Redirect />}> */}
-					<Route element={<Redirect />}>
-						<Route path="/" element={<LandingPage />} />
-						<Route path="/login" element={<LoginPage />} />
-					</Route>
-
-					<Route path="/register" element={<RegisterPage />}>
-						<Route path="/register/form" element={<RegisterForm />} />
-						<Route path="/register/otp" element={<RegisterVerifyOtp />} />
-					</Route>
-					{/* </Route> */}
-					<Route element={<Persist />}>
-						<Route element={<RouteGuard />}>
-							{/* Channel */}
-							<Route path="/chat" element={<ChannelListScrren />}>
-								<Route path="/chat/:channelid" element={<ChatBox />} />
-							</Route>
-							{/* User list */}
-							<Route path="/u" element={<UserListScreen />}>
-								<Route path="/u/:channelid" element={<ChatBox />} />
-							</Route>
-							{/* Group List */}
-							<Route path="/g" element={<GroupListScreen />}>
-								<Route path="/g/:channelid" element={<ChatBox />} />
-							</Route>
-							<Route path="/avatar" element={<AvatarScreen />}>
-								<Route path="/avatar/upload" element={<UploadAvatar />} />
-								<Route path="/avatar/remove" element={<RemovedAvatar />} />
-							</Route>
-						</Route>
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</Suspense>
-	);
+          <Route path="/register" element={<RegisterPage />}>
+            <Route path="/register/form" element={<RegisterForm />} />
+            <Route path="/register/otp" element={<RegisterVerifyOtp />} />
+          </Route>
+          {/* </Route> */}
+          <Route element={<Persist />}>
+            <Route element={<RouteGuard />}>
+              {/* Channel */}
+              <Route path="/chat" element={<ChannelListScrren />}>
+                <Route path="/chat/:channelid" element={<ChatBox />} />
+              </Route>
+              {/* Group List */}
+              <Route path="/g" element={<GroupListScreen />}>
+                <Route path="/g/:channelid" element={<ChatBox />} />
+              </Route>
+              <Route path="/avatar" element={<AvatarScreen />}>
+                <Route path="/avatar/upload" element={<UploadAvatar />} />
+                <Route path="/avatar/remove" element={<RemovedAvatar />} />
+              </Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
+  );
 }
