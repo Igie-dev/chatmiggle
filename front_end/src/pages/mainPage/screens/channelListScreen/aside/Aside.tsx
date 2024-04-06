9f730b563af179584b7a2c6b55b0fd01c9d52441
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-07 00:32:03 +0800

    Add button toggle

--- front_end/src/pages/mainPage/screens/channelListScreen/aside/Aside.tsx
+++ front_end/src/pages/mainPage/screens/channelListScreen/aside/Aside.tsx
@@ -2,7 +2,7 @@ import { ForwardedRef } from "react";
 import Header from "../../../../../components/shared/Header";
 import ChannelList from "./ChannelList";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { ChevronLeft, ChevronRight, Plus } from "lucide-react";
 
 type Props = {
   handleAside: () => void;
@@ -12,9 +12,27 @@ export default function AsideNav({ handleAside, asideRef }: Props) {
   return (
     <aside
       ref={asideRef}
-      className="absolute top-0 left-0 z-50 w-full h-full bg-primary-forground/50  transition-all lg:static lg:translate-x-0 lg:w-[22rem]"
+      className="absolute top-0 left-0 z-50 w-full h-full bg-background/90  transition-all lg:static lg:translate-x-0 lg:w-[20rem]"
     >
-      <div className="flex flex-col h-full w-[20rem] gap-2 py-5 px-4 bg-background/50 border-r lg:w-full relative lg:border-none">
+      {/* Close nav button */}
+      <Button
+        onClick={handleAside}
+        size="icon"
+        variant="outline"
+        className="absolute top-1 -right-11 lg:hidden"
+      >
+        <ChevronRight size={20} />
+      </Button>
+      <div className="flex flex-col h-full w-[22rem] gap-2 py-5  px-4 bg-background/50 border-r lg:w-full relative lg:border-none">
+        {/* Open nav button */}
+        <Button
+          onClick={handleAside}
+          size="icon"
+          variant="outline"
+          className="absolute top-1 -right-12 lg:hidden"
+        >
+          <ChevronLeft size={20} />
+        </Button>
         <Header />
         <ChannelList handleAside={handleAside} />
         <Button
--- front_end/src/pages/mainPage/screens/groupListScreen/aside/Aside.tsx
+++ front_end/src/pages/mainPage/screens/groupListScreen/aside/Aside.tsx
@@ -2,7 +2,7 @@ import { ForwardedRef } from "react";
 import Header from "../../../../../components/shared/Header";
 import GroupList from "./GroupList";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { ChevronLeft, ChevronRight, Plus } from "lucide-react";
 type Props = {
   handleAside: () => void;
   asideRef: ForwardedRef<HTMLElement | null>;
@@ -11,9 +11,27 @@ export default function AsideNav({ handleAside, asideRef }: Props) {
   return (
     <aside
       ref={asideRef}
-      className="absolute top-0 left-0 z-50 w-full h-full  bg-primary-forground/50 lg:static lg:translate-x-0  transition-all lg:w-[22rem]"
+      className="absolute top-0 left-0 z-50 w-full h-full bg-background/90 lg:static lg:translate-x-0  transition-all lg:w-[20rem]"
     >
-      <div className="flex flex-col h-full w-[20rem] py-5 px-4 gap-2 bg-background/50 border-r lg:w-full lg:border-none relative">
+      {/* Close nav button */}
+      <Button
+        onClick={handleAside}
+        size="icon"
+        variant="outline"
+        className="absolute top-1 -right-11 lg:hidden"
+      >
+        <ChevronRight size={20} />
+      </Button>
+      <div className="flex flex-col h-full w-[22rem] py-5 px-4 gap-2 bg-background/50 border-r lg:w-full lg:border-none relative">
+        {/* Open nav button */}
+        <Button
+          onClick={handleAside}
+          size="icon"
+          variant="outline"
+          className="absolute top-1 -right-12 lg:hidden"
+        >
+          <ChevronLeft size={20} />
+        </Button>
         <Header />
         <GroupList handleAside={handleAside} />
         <Button
