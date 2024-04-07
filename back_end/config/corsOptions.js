dfcac2138a781f012bb2e078d089fa959131374e
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-08 00:16:07 +0800

    add block

--- back_end/config/corsOptions.js
+++ back_end/config/corsOptions.js
@@ -1,14 +1,14 @@
 import allowedOrigins from "./allowedOrigins.js";
 const corsOptions = {
-	origin: (origin, callback) => {
-		if (true) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not Allowed by Cors"));
-		}
-	},
-	credentials: true, // Corrected spelling here
-	optionsSuccessStatus: 200,
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // Corrected spelling here
+  optionsSuccessStatus: 200,
 };
 
 export default corsOptions;
