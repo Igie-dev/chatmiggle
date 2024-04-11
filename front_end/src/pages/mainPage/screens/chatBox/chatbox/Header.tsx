0263d980463bef0d414a64fce29aa0eacf8e4a4a
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-11 14:00:05 +0800

    Update chatbox header

--- front_end/src/pages/mainPage/screens/chatBox/chatbox/Header.tsx
+++ front_end/src/pages/mainPage/screens/chatBox/chatbox/Header.tsx
@@ -1,47 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { EllipsisVertical } from "lucide-react";
-import { useParams } from "react-router-dom";
-import { useGetChannelQuery } from "@/service/slices/channel/channelApiSlice";
-import { useAppSelector } from "@/service/store";
-import { getCurrentUser } from "@/service/slices/user/userSlice";
-import UserAvatar from "@/components/shared/UserAvatar";
-import GroupAvatar from "@/components/shared/GroupAvatar";
-import DisplayUserName from "../../channelScreen/aside/channelCard/DisplayUserName";
 type Props = {
   handleAside: () => void;
 };
 export default function Header({ handleAside }: Props) {
-  const { channelId } = useParams();
-  const { data } = useGetChannelQuery(channelId as string);
-  const { user_id } = useAppSelector(getCurrentUser);
-  const mateId =
-    data?.members?.length >= 3
-      ? ""
-      : data?.members[0].user_id !== user_id
-      ? data?.members[0].user_id
-      : data?.members[1].user_id;
   return (
-    <header className="flex items-center justify-between w-full h-20 px-2">
-      <div className="flex items-center flex-1 gap-2 ml-10 lg:ml-0">
-        <div className="w-11 h-11">
-          {data?.isPrivate ? (
-            <UserAvatar userId={mateId} />
-          ) : (
-            <GroupAvatar channelId={data?.channel_id} />
-          )}
-        </div>
-        <div className="w-[12rem] lg:w-[15rem]">
-          {data?.isPrivate ? (
-            <span className="w-full text-sm font-semibold truncate opacity-90 max-h-6">
-              <DisplayUserName userId={mateId} />
-            </span>
-          ) : (
-            <p className="w-full text-sm font-semibold truncate opacity-90 max-h-6">
-              {data?.group_name}
-            </p>
-          )}
-        </div>
-      </div>
+    <header className="flex items-center justify-between w-full h-20 px-2 ">
+      <div>Header</div>
       <div>
         <Button
           onClick={handleAside}
