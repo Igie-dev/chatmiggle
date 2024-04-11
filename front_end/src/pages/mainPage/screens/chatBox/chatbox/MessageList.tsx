fea9b5aa19cefeabbd51650d6b891ad30e390485
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-12 00:37:38 +0800

    Add scroll effect

--- front_end/src/pages/mainPage/screens/chatBox/chatbox/MessageList.tsx
+++ front_end/src/pages/mainPage/screens/chatBox/chatbox/MessageList.tsx
@@ -1,22 +1,72 @@
-import { useGetChannelMessagesQuery } from "@/service/slices/channel/channelApiSlice";
-// import { useState } from "react";
 import { useParams } from "react-router-dom";
 import LoaderSpinner from "@/components/loader/LoaderSpinner";
 import MessageCard from "./MessageCard";
 import { MessageSquare } from "lucide-react";
+import useChannelMessages from "@/hooks/useChannelMessages";
+import { useEffect } from "react";
+
 export default function MessageList() {
   const { channelId } = useParams();
-  // const [cursor, setCursor] = useState("");
-  const { data, isFetching, isError } = useGetChannelMessagesQuery({
-    channelId: channelId as string,
-  });
+  const { messages, isFetching, isError, refetch } = useChannelMessages(
+    channelId as string
+  );
+
+  //handle scroll to bottom
+  useEffect(() => {
+    if (messages?.length >= 5) {
+      const targetEl = document.getElementById(
+        `${messages[messages.length - 1]?.message_id}`
+      ) as HTMLElement;
+      targetEl?.lastElementChild?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
+
+  //Handle refetch when view on top
+  useEffect(() => {
+    if (messages?.length <= 99) return;
+    const el = document.getElementById(
+      `${messages[0]?.message_id}`
+    ) as HTMLElement;
+    const observer = new window.IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          refetch();
+          console.log("Show");
+        }
+      },
+      {
+        root: null,
+        threshold: 0,
+      }
+    );
+
+    const interval = setInterval(() => {
+      observer.observe(el);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [messages, refetch]);
+
   return (
-    <div className="w-full h-[80%] overflow-auto ">
+    <div className="relative flex-1 w-full overflow-auto">
+      <div
+        className={`sticky top-0 left-0 z-40 flex justify-center w-full py-2  transition-all ${
+          messages?.length >= 100 && isFetching ? "visible" : "hidden"
+        }`}
+      >
+        <p className="px-4 py-2 text-sm border rounded-full bg-background/80">
+          Please wait...
+        </p>
+      </div>
       <ul className="flex flex-col w-full gap-5 px-4 py-2 pb-20 h-fit">
-        {isFetching ? (
+        {messages?.length <= 0 && isFetching ? (
           <LoaderSpinner />
-        ) : data?.messages?.length >= 1 && !isError ? (
-          data.messages.map((message: TMessageData) => {
+        ) : messages?.length >= 1 && !isError ? (
+          messages.map((message: TMessageData) => {
             return <MessageCard key={message.message_id} message={message} />;
           })
         ) : (
