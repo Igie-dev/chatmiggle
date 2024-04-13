7d56e906813b654014f05137b5486ec581516126
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-13 23:16:06 +0800

    Update socket

--- back_end/socket/socket.js
+++ back_end/socket/socket.js
@@ -36,31 +36,51 @@ const socketConnection = (httpServer) => {
   //Connection
   io.on("connection", (socket) => {
     // console.log("Socket connnected");
-    socket.on("user_join", async (user_id) => {
+    socket.on("user_join", (user_id) => {
       socket.join(user_id);
     });
 
-    socket.on("createNewChannel", ({ members, message, sender_id, type }) => {
-      createNewChannel({ members, message, sender_id, type })
-        .then((res) => {
-          if (res?.data) {
-            members.forEach((m) => {
-              io.to(m.user_id).emit("new_channel", {
+    /// Active channel
+    socket.on("active_channel", (channe_id) => {
+      socket.join(channe_id);
+    });
+
+    //Listen for create new message on client
+    //The event on front end when using the create private message
+    socket.on(
+      "create_new_channel",
+      async ({ members, message, sender_id, type }) => {
+        //This function return a promise and handle
+        //the storing of message data on database
+        //it return the data when it successfully saved
+        createNewChannel({ members, message, sender_id, type })
+          .then((res) => {
+            if (res?.data) {
+              //Emit all the members of the created private message
+              members.forEach((m) => {
+                io.to(m.user_id).emit("new_channel", {
+                  data: res.data,
+                });
+              });
+              //Send an emit to client where the channel id was listened
+              socket.to(res?.data?.channel_id).emit("new_channel_message", {
                 data: res.data,
               });
+              //Return to sender
+              io.to(socket.id).emit("create_new_channel", {
+                data: res?.data?.channel_id,
+              });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            //Return to sender
+            io.to(socket.id).emit("create_new_channel", {
+              error: "Somthing went wrong",
             });
-
-            io.to(res?.data?.channel_id).emit("new_channel_message", {
-              data: res.data,
-            });
-            io.emit("createNewChannel", { data: res?.data?.channel_id });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          io.emit("createNewChannel", { error: "Somthing went wrong" });
-        });
-    });
+          });
+      }
+    );
   });
 };
 
