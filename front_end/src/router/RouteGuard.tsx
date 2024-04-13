120fa9696d43069445f17237b4fc143dc5cbce16
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-13 23:16:39 +0800

    update

--- front_end/src/router/RouteGuard.tsx
+++ front_end/src/router/RouteGuard.tsx
@@ -3,12 +3,12 @@ import { getCurrentToken } from "@/service/slices/auth/authSlice";
 import { getCurrentUser } from "@/service/slices/user/userSlice";
 import { Outlet, Navigate, useLocation } from "react-router-dom";
 export default function RouteGuard() {
-	const location = useLocation();
-	const token = useAppSelector(getCurrentToken);
-	const user = useAppSelector(getCurrentUser);
+  const location = useLocation();
+  const token = useAppSelector(getCurrentToken);
+  const user = useAppSelector(getCurrentUser);
 
-	if (!token || !user.user_id) {
-		return <Navigate to="/logion" state={{ from: location }} replace />;
-	}
-	return <Outlet />;
+  if (!token || !user.user_id) {
+    return <Navigate to="/logion" state={{ from: location }} replace />;
+  }
+  return <Outlet />;
 }
