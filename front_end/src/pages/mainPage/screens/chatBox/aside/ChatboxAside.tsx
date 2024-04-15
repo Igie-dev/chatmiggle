af61d76e862dbb00bb26131e38e330dbc55741a9
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-16 01:46:48 +0800

    Change background

--- front_end/src/pages/mainPage/screens/chatBox/aside/ChatboxAside.tsx
+++ front_end/src/pages/mainPage/screens/chatBox/aside/ChatboxAside.tsx
@@ -8,7 +8,7 @@ export default function ChatboxAside({ asideRef, handleAside }: Props) {
   return (
     <aside
       ref={asideRef}
-      className="h-full flex flex-col p-1  md:border md:rounded-lg w-full absolute top-0 right-0 xl:static bg-background/90  xl:translate-x-0 transition-all translate-x-full xl:w-[22rem] 2xl:w-[24rem]"
+      className="h-full flex flex-col p-1  md:border md:rounded-lg w-full absolute top-0 right-0 xl:static bg-background  xl:translate-x-0 transition-all translate-x-full xl:w-[22rem] 2xl:w-[24rem]"
     >
       <Header handleAside={handleAside} />
     </aside>
