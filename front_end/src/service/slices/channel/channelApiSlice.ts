028e702dd921b1286a75651ce49fce376102099a
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-16 01:44:42 +0800

    Update

--- front_end/src/service/slices/channel/channelApiSlice.ts
+++ front_end/src/service/slices/channel/channelApiSlice.ts
@@ -2,33 +2,51 @@
 import { apiSlice } from "../api/apiSlice";
 
 const channelApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        getUserChannels: builder.query({
-            query: (user_id:string) => ({ 
-                url: `/channel/userchannel/${user_id}`,
-                method: "GET",
-            })
-        }),
-        verifyUserInChannel: builder.query({
-            query: ({channel_id,user_id}:{channel_id:string,user_id:string}) => ({
-                url: `/channel/verifyuser/${channel_id}/${user_id}`,
-                method: "GET"
-            })
-        }),
-        getChannelMessages: builder.query({
-            query: ({channelId,cursor}:{channelId:string,cursor?:string}) => ({
-                url: `/channel/messages/${channelId}?take=100${cursor ? `&cursor=${cursor}` : ""}`,
-                method: "GET"
-            })
-        }),
-        getChannel: builder.query({
-            query:(channelId:string) => ({
-                url: `/channel/${channelId}`,
-                method: "GET"
-            })
-        })
-    })
-})
+  endpoints: (builder) => ({
+    getUserChannels: builder.query({
+      query: (user_id: string) => ({
+        url: `/channel/userchannel/${user_id}`,
+        method: "GET",
+      }),
+    }),
+    verifyUserInChannel: builder.query({
+      query: ({
+        channel_id,
+        user_id,
+      }: {
+        channel_id: string;
+        user_id: string;
+      }) => ({
+        url: `/channel/verifyuser/${channel_id}/${user_id}`,
+        method: "GET",
+      }),
+    }),
+    getChannelMessages: builder.query({
+      query: ({
+        channelId,
+        cursor,
+      }: {
+        channelId: string;
+        cursor?: number | null;
+      }) => ({
+        url: `/channel/messages/${channelId}?take=100${
+          cursor ? `&cursor=${cursor}` : ""
+        }`,
+        method: "GET",
+      }),
+    }),
+    getChannel: builder.query({
+      query: (channelId: string) => ({
+        url: `/channel/${channelId}`,
+        method: "GET",
+      }),
+    }),
+  }),
+});
 
-
-export const {useGetUserChannelsQuery,useVerifyUserInChannelQuery,useGetChannelMessagesQuery,useGetChannelQuery} = channelApiSlice;
+export const {
+  useGetUserChannelsQuery,
+  useVerifyUserInChannelQuery,
+  useGetChannelMessagesQuery,
+  useGetChannelQuery,
+} = channelApiSlice;
