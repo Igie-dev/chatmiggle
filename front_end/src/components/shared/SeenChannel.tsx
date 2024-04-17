39a1abad3bd4246a6bcebc47be1a1e93a708a53b
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-17 23:33:30 +0800

    Add seen component

---
+++ front_end/src/components/shared/SeenChannel.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { socket } from "@/socket";
+import { useAppSelector } from "@/service/store";
+import { getCurrentUser } from "@/service/slices/user/userSlice";
+type Props = {
+  channel: TChannelData;
+};
+export default function SeenChannel({ channel }: Props) {
+  const { user_id } = useAppSelector(getCurrentUser);
+  const [isSeen, setIsSeen] = useState(false);
+
+  useEffect(() => {
+    if (channel.messages[0]?.message_id && channel.members.length >= 1) {
+      const user = channel.members.filter(
+        (m) => m.user_id === user_id && m.is_seen
+      );
+
+      const isSeenByUser = user.length >= 1;
+      setIsSeen(isSeenByUser);
+    }
+  }, [user_id, channel.members, channel.messages]);
+
+  useEffect(() => {
+    socket.emit("listen_seen_channel", channel.channel_id);
+    socket.on("seen_channel", (res: { data: TChannelData }) => {
+      console.log(res?.data);
+      if (res?.data.channel_id === channel.channel_id && !isSeen) {
+        setIsSeen(true);
+      }
+    });
+
+    return () => {
+      socket.off("seen_channel");
+    };
+  }, [channel?.channel_id, isSeen]);
+
+  return channel.messages[0].sender_id !== user_id ? (
+    <>
+      {isSeen ? null : (
+        <p className="absolute top-0 right-1 text-[10px] opacity-50">New</p>
+      )}
+    </>
+  ) : null;
+}
--- front_end/src/pages/mainPage/screens/channelScreen/aside/channelCard/ChannelCard.tsx
+++ front_end/src/pages/mainPage/screens/channelScreen/aside/channelCard/ChannelCard.tsx
@@ -4,6 +4,7 @@ import UserAvatar from "@/components/shared/UserAvatar";
 import { isToday, formatTime, formatDate } from "@/lib/dateFormat";
 import { useNavigate, useParams } from "react-router-dom";
 import DisplayUserName from "./DisplayUserName";
+import SeenChannel from "@/components/shared/SeenChannel";
 type Props = {
   channel: TChannelData;
 };
@@ -11,11 +12,12 @@ export default function ChannelCard({ channel }: Props) {
   const { user_id } = useAppSelector(getCurrentUser);
   const navigate = useNavigate();
   const { channelId } = useParams();
+
   const mate_id =
     channel?.members[0]?.user_id === user_id
       ? channel?.members[1]?.user_id
       : channel?.members[0]?.user_id;
-  const today = isToday(channel.messages[0]?.createdAt);
+  const today = isToday(channel?.messages[0]?.createdAt);
 
   const handleClick = () => {
     navigate(`/c/${channel.channel_id}`);
@@ -24,7 +26,7 @@ export default function ChannelCard({ channel }: Props) {
   return (
     <li
       onClick={handleClick}
-      className={`flex items-start w-full gap-3 p-2 transition-all border rounded-md cursor-pointer h-fit ${
+      className={`flex items-start w-full gap-3 p-2 transition-all border rounded-md relative cursor-pointer h-fit ${
         channelId === channel.channel_id
           ? "bg-primary-foreground border-border/70"
           : "bg-transparent border-transparent hover:shadow-md hover:bg-primary-foreground"
@@ -33,18 +35,18 @@ export default function ChannelCard({ channel }: Props) {
       <div className="w-9 h-9">
         <UserAvatar userId={mate_id} />
       </div>
-      <div className="flex flex-col w-[80%] h-full justify-center">
+      <div className={`flex flex-col w-[80%] h-full justify-center `}>
         {channel?.isPrivate ? (
-          <span className="w-full max-w-full text-sm font-semibold truncate opacity-90 max-h-6">
+          <span className="w-full max-w-full text-sm truncate opacity-90 max-h-6">
             <DisplayUserName userId={mate_id} />
           </span>
         ) : (
-          <p className="w-full max-w-full text-sm font-semibold truncate opacity-90 max-h-6">
+          <p className="w-full max-w-full text-sm truncate opacity-90 max-h-6">
             {channel?.group_name}
           </p>
         )}
 
-        <div className="flex items-end w-full ">
+        <div className="flex items-end w-full">
           <p className="w-fit max-w-[50%] text-xs truncate opacity-70 max-h-6">
             {channel.messages[0]?.sender_id === user_id
               ? `You: ${channel.messages[0]?.message}`
@@ -59,6 +61,7 @@ export default function ChannelCard({ channel }: Props) {
           </p>
         </div>
       </div>
+      <SeenChannel channel={channel} />
     </li>
   );
 }
