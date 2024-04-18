4bca44db8bc8b29018826cd538f2a192b86d6526
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-18 18:33:51 +0800

    Change props

--- front_end/src/components/shared/SeenChannel.tsx
+++ front_end/src/components/shared/SeenChannel.tsx
@@ -3,27 +3,24 @@ import { socket } from "@/socket";
 import { useAppSelector } from "@/service/store";
 import { getCurrentUser } from "@/service/slices/user/userSlice";
 type Props = {
-  channel: TChannelData;
+  members: TChannelMemberData[];
+  senderId: string;
+  messageId: string;
 };
-export default function SeenChannel({ channel }: Props) {
+export default function SeenChannel({ members, senderId, messageId }: Props) {
   const { user_id } = useAppSelector(getCurrentUser);
   const [isSeen, setIsSeen] = useState(false);
-
   useEffect(() => {
-    if (channel.messages[0]?.message_id && channel.members.length >= 1) {
-      const user = channel.members.filter(
-        (m) => m.user_id === user_id && m.is_seen
-      );
-
-      const isSeenByUser = user.length >= 1;
+    if (members.length >= 1) {
+      const users = members.filter((m) => m.user_id === user_id && m.is_seen);
+      const isSeenByUser = users.length >= 1;
       setIsSeen(isSeenByUser);
     }
-  }, [user_id, channel.members, channel.messages]);
+  }, [user_id, members, messageId, senderId]);
 
   useEffect(() => {
-    socket.emit("listen_seen_channel", channel.channel_id);
     socket.on("seen_channel", (res: { data: TChannelData }) => {
-      if (res?.data.channel_id === channel.channel_id && !isSeen) {
+      if (res?.data.channel_id === members[0].channel_id && !isSeen) {
         setIsSeen(true);
       }
     });
@@ -31,12 +28,12 @@ export default function SeenChannel({ channel }: Props) {
     return () => {
       socket.off("seen_channel");
     };
-  }, [channel?.channel_id, isSeen]);
+  }, [members, isSeen]);
 
-  return channel.messages[0].sender_id !== user_id ? (
+  return senderId !== user_id ? (
     <>
       {isSeen ? null : (
-        <p className="absolute top-0 right-1 text-[10px] opacity-50">New</p>
+        <p className="absolute top-0 right-2 text-[10px] opacity-50">New</p>
       )}
     </>
   ) : null;
