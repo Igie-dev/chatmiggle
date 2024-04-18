7c9c05dcd0e25d232ec84aaf3f9dcaeedc4ec05c
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-18 23:28:27 +0800

    make mutation

--- front_end/src/service/slices/user/userApiSlice.ts
+++ front_end/src/service/slices/user/userApiSlice.ts
@@ -2,40 +2,47 @@
 import { apiSlice } from "../api/apiSlice";
 
 const userApiSlice = apiSlice.injectEndpoints({
-	endpoints: (builder) => ({
-		getUsers: builder.query({
-			query: (cursor: number) => ({
-				url: `/user?take=40&cursor=${cursor}`,
-				method: "GET",
-			}),
-			providesTags: ["user"],
-		}),
-		getUserById: builder.query({
-			query: (id: string) => ({
-				url: `/user/${id}`,
-				method: "GET",
-			}),
-		}),
-		deleteUserById: builder.query({
-			query: (id: string) => ({
-				url: `/user/${id}`,
-				method: "DELETE",
-			}),
-		}),
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: (cursor: number) => ({
+        url: `/user?take=40&cursor=${cursor}`,
+        method: "GET",
+      }),
+      providesTags: ["user"],
+    }),
+    getUserById: builder.query({
+      query: (id: string) => ({
+        url: `/user/${id}`,
+        method: "GET",
+      }),
+    }),
+    deleteUserById: builder.query({
+      query: (id: string) => ({
+        url: `/user/${id}`,
+        method: "DELETE",
+      }),
+    }),
 
-		updateUser: builder.mutation({
-			query: (user: TUpdateUser) => ({
-				url: `/user`,
-				method: "PATCH",
-				data: { ...user },
-			}),
-		}),
-	}),
+    updateUser: builder.mutation({
+      query: (user: TUpdateUser) => ({
+        url: `/user`,
+        method: "PATCH",
+        data: { ...user },
+      }),
+    }),
+    getUserByIdMut: builder.mutation({
+      query: (id: string) => ({
+        url: `/user/${id}`,
+        method: "GET",
+      }),
+    }),
+  }),
 });
 
 export const {
-	useGetUsersQuery,
-	useGetUserByIdQuery,
-	useDeleteUserByIdQuery,
-	useUpdateUserMutation,
+  useGetUsersQuery,
+  useGetUserByIdQuery,
+  useGetUserByIdMutMutation,
+  useDeleteUserByIdQuery,
+  useUpdateUserMutation,
 } = userApiSlice;
