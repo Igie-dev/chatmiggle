b393d111c9e42d7843aa58e0428363071411dffb
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-24 23:46:25 +0800

    Change layout

---
+++ front_end/src/pages/authPage/register/registerForm/RegisterForm.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { Outlet, useNavigate } from "react-router-dom";
+import { X } from "lucide-react";
+export default function RegisterForm() {
+  const navigate = useNavigate();
+  const handleCancel = () => {
+    localStorage.removeItem("firstName");
+    localStorage.removeItem("lastName");
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    navigate("/login");
+  };
+  return (
+    <div className="w-full max-w-[30rem] flex flex-col items-center gap-5  px-2 py-2 rounded-md  relative border">
+      <header className="relative flex flex-col items-center w-full gap-3 pt-5">
+        <Button
+          size="icon"
+          onClick={handleCancel}
+          variant="ghost"
+          className="absolute top-0 left-2"
+        >
+          <X size={20} />
+        </Button>
+        <h5 className="text-lg font-semibold">Create your account</h5>
+        <h1 className="text-lg font-black">Sign up</h1>
+      </header>
+      <Outlet />
+      <div className="flex items-center justify-center w-full gap-2 px-5 pt-5 pb-5 text-sm">
+        <p>Already have an account?</p>
+        <button onClick={handleCancel} className="text-blue-500">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
