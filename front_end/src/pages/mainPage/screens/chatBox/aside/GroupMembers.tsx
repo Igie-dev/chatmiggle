16852693629c0a1b1eee6ad5cb415e1c12cb1279
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-28 03:04:54 +0800

    update

--- front_end/src/pages/mainPage/screens/chatBox/aside/GroupMembers.tsx
+++ front_end/src/pages/mainPage/screens/chatBox/aside/GroupMembers.tsx
@@ -1,29 +1,29 @@
-import { Button } from "@/components/ui/button";
-import { ChevronLeft } from "lucide-react";
-import { Dispatch, SetStateAction } from "react";
 import MemberCard from "./MemberCard";
+import AddMember from "./AddMember";
+import LeaveGroup from "./LeaveGroup";
 type Props = {
   members: TChannelMemberData[];
-  setIsOpenMembers: Dispatch<SetStateAction<boolean>>;
+  channelId: string;
+  groupName: string;
 };
-export default function GroupMembers({ members, setIsOpenMembers }: Props) {
+export default function GroupMembers({ members, channelId, groupName }: Props) {
   return (
-    <div className="absolute top-0 left-0 flex flex-col w-full h-full gap-2 bg-background">
-      <div className="flex items-center">
-        <Button
-          size="icon"
-          variant="ghost"
-          onClick={() => setIsOpenMembers(false)}
-        >
-          <ChevronLeft size={20} />
-        </Button>
+    <div className="absolute top-0 left-0 flex flex-col w-full h-full gap-2 p-2 bg-background">
+      <div className="flex items-center justify-between pt-2 pb-4 border-b">
+        <AddMember channelId={channelId} groupName={groupName} />
+        <LeaveGroup channelId={channelId} groupName={groupName} />
       </div>
       <div className="w-full h-[95%] overflow-auto">
-        <ul className="flex flex-col w-full px-4 py-5 h-fit">
+        <ul className="flex flex-col w-full pb-5 h-fit">
           {members?.length >= 1
             ? members.map((member) => {
                 return (
-                  <MemberCard key={member.user_id} userId={member.user_id} />
+                  <MemberCard
+                    key={member.user_id}
+                    channelId={channelId}
+                    groupName={groupName}
+                    userId={member.user_id}
+                  />
                 );
               })
             : null}
