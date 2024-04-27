371461aa816b518b74d38147b7df818043c635aa
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-28 03:04:32 +0800

    update

--- front_end/src/pages/mainPage/screens/chatBox/aside/ChatboxAside.tsx
+++ front_end/src/pages/mainPage/screens/chatBox/aside/ChatboxAside.tsx
@@ -1,79 +1,50 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Header from "./Header";
 import { useGetChannelQuery } from "@/service/slices/channel/channelApiSlice";
-import { Button } from "@/components/ui/button";
-import { ChevronRight } from "lucide-react";
-import { useState } from "react";
 import GroupMembers from "./GroupMembers";
-import LeaveGroup from "./LeaveGroup";
-import AddMember from "./AddMember";
-type Props = {
-  asideRef: React.ForwardedRef<HTMLDivElement | null>;
-  handleAside: () => void;
-};
-export default function ChatboxAside({ asideRef, handleAside }: Props) {
+import { useRef } from "react";
+import { Button } from "@/components/ui/button";
+import { EllipsisVertical, X } from "lucide-react";
+export default function ChatboxAside() {
+  const asideRef = useRef<HTMLDivElement | null>(null);
   const { channelId } = useParams();
   const { data, isFetching } = useGetChannelQuery(channelId as string);
-  const [isOpenMembers, setIsOpenMembers] = useState(false);
+  const handleAside = () => {
+    if (asideRef?.current?.classList.contains("translate-x-full")) {
+      asideRef?.current?.classList.remove("translate-x-full");
+    } else {
+      asideRef?.current?.classList.add("translate-x-full");
+    }
+  };
+
   return (
     <aside
       ref={asideRef}
-      className="h-full flex flex-col p-1   md:border md:rounded-lg w-full absolute top-0 right-0 xl:static bg-background  xl:translate-x-0 transition-all translate-x-full xl:w-[22rem] 2xl:w-[24rem]"
+      className=" h-full flex flex-col p-1 gap-1 md:border md:rounded-lg w-full absolute top-0 right-0 xl:static bg-background  xl:translate-x-0 transition-all translate-x-full xl:w-[22rem] 2xl:w-[24rem]"
     >
-      <Header
-        handleAside={handleAside}
-        channel={data}
-        isFetching={isFetching}
-      />
-      <div className="w-full h-[75%] relative mt-10  overflow-auto">
-        {data && !isFetching ? (
-          <>
-            <div className="flex flex-col w-full h-fit">
-              {!data?.is_private ? (
-                <Button variant="ghost" size="icon" asChild>
-                  <Link
-                    to={`/avatar/upload/${data?.channel_id}`}
-                    className="flex justify-between w-full px-4"
-                  >
-                    <p>Change group picture</p>
-                    <ChevronRight size={20} />
-                  </Link>
-                </Button>
-              ) : null}
-              {!data?.is_private ? (
-                <AddMember
-                  channelId={data?.channel_id}
-                  groupName={data?.group_name}
-                />
-              ) : null}
-
-              {!data?.is_private ? (
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => setIsOpenMembers(true)}
-                  className="flex justify-between w-full px-4"
-                >
-                  <p>Members</p>
-                  <ChevronRight size={20} />
-                </Button>
-              ) : null}
-              {!data?.is_private ? (
-                <LeaveGroup
-                  channelId={data?.channel_id}
-                  groupName={data?.group_name}
-                />
-              ) : null}
-            </div>
-          </>
-        ) : null}
-
-        {isOpenMembers ? (
-          <GroupMembers
-            members={data?.members}
-            setIsOpenMembers={setIsOpenMembers}
-          />
-        ) : null}
+      <Button
+        onClick={handleAside}
+        size="icon"
+        variant="ghost"
+        className="absolute -left-12 top-2 xl:hidden"
+      >
+        <EllipsisVertical size={20} />
+      </Button>
+      <Button
+        onClick={handleAside}
+        size="icon"
+        variant="ghost"
+        className="absolute right-2 top-2 xl:hidden"
+      >
+        <X size={20} />
+      </Button>
+      <Header channel={data} isFetching={isFetching} />
+      <div className="relative flex-1 w-full px-2 pt-5 overflow-auto border rounded-md bg-accent/50">
+        <GroupMembers
+          members={data?.members}
+          channelId={data?.channel_id}
+          groupName={data?.group_name}
+        />
       </div>
     </aside>
   );
