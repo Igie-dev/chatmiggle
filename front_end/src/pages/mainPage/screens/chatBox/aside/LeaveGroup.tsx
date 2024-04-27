2e821d6db6ced4e90bca90878bb64a8354cd0505
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-27 23:36:48 +0800

    leave group

--- front_end/src/pages/mainPage/screens/chatBox/aside/LeaveGroup.tsx
+++ front_end/src/pages/mainPage/screens/chatBox/aside/LeaveGroup.tsx
@@ -9,15 +9,34 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { ChevronRight } from "lucide-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import DisplayAvatar from "@/components/shared/DisplayAvatar";
+import { useAppSelector } from "@/service/store";
+import { getCurrentUser } from "@/service/slices/user/userSlice";
+import { useRemoveUserFromChannelMutation } from "@/service/slices/channel/channelApiSlice";
+import BtnsLoaderSpinner from "@/components/loader/BtnLoader";
 type Props = {
   channelId: string;
   groupName: string;
 };
 export default function LeaveGroup({ channelId, groupName }: Props) {
   const [open, setOpen] = useState(false);
-  //TODO make user to leave group
+  const { user_id } = useAppSelector(getCurrentUser);
+  console.log(user_id);
+  const [leave, { isLoading, error }] = useRemoveUserFromChannelMutation();
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const res: any = await leave({ userId: user_id, channelId });
+      if (res?.data) {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
@@ -31,25 +50,29 @@ export default function LeaveGroup({ channelId, groupName }: Props) {
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader className="items-center">
-          <DialogTitle>Leave Group</DialogTitle>
-          <DialogDescription>
-            Are you sure you want to leave this group?
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col items-center justify-center gap-2 my-5">
-          <div className="w-16 h-16 overflow-hidden border rounded-full">
-            <DisplayAvatar id={channelId} />
+        <form onSubmit={handleSubmit}>
+          <DialogHeader className="items-center">
+            <DialogTitle>Leave Group</DialogTitle>
+            <DialogDescription>
+              Are you sure you want to leave this group?
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col items-center justify-center gap-2 my-5">
+            <div className="w-16 h-16 overflow-hidden border rounded-full">
+              <DisplayAvatar id={channelId} />
+            </div>
+            <span className="w-full font-normal text-center truncate text-medium">
+              {groupName}
+            </span>
+            <p className="text-sm text-destructive">{error?.data?.message}</p>
           </div>
-          <span className="w-full font-normal text-center truncate text-medium">
-            {groupName}
-          </span>
-        </div>
-        <DialogFooter>
-          <Button type="submit" className="w-full">
-            Leave
-          </Button>
-        </DialogFooter>
+
+          <DialogFooter>
+            <Button type="submit" disabled={isLoading} className="w-full">
+              {isLoading ? <BtnsLoaderSpinner /> : "Leave"}
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
