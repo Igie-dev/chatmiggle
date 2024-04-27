a75d7aec0f994658f84cf9e30520f4857feb2db4
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-28 03:03:11 +0800

    Update

--- front_end/src/pages/mainPage/screens/chatBox/ChatBoxContainer.tsx
+++ front_end/src/pages/mainPage/screens/chatBox/ChatBoxContainer.tsx
@@ -1,22 +1,11 @@
-import { useRef } from "react";
 import ChatboxAside from "./aside/ChatboxAside";
 import ChatBox from "./chatbox/ChatBox";
 export default function ChatBoxContainer() {
-  const asideRef = useRef<HTMLDivElement | null>(null);
-
-  const handleAside = () => {
-    if (asideRef?.current?.classList.contains("translate-x-full")) {
-      asideRef?.current?.classList.remove("translate-x-full");
-    } else {
-      asideRef?.current?.classList.add("translate-x-full");
-    }
-  };
-
   return (
     <section className="relative flex items-center w-full h-full gap-2">
       <div className="relative flex items-center w-full h-full gap-2 overflow-hidden">
-        <ChatBox handleAside={handleAside} />
-        <ChatboxAside asideRef={asideRef} handleAside={handleAside} />
+        <ChatBox />
+        <ChatboxAside />
       </div>
     </section>
   );
