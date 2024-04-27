4d9499bc433133b08c9dcdacf3400eae16992fff
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-28 03:03:29 +0800

    Remove header

--- front_end/src/pages/mainPage/screens/chatBox/chatbox/ChatBox.tsx
+++ front_end/src/pages/mainPage/screens/chatBox/chatbox/ChatBox.tsx
@@ -1,14 +1,8 @@
 import ChatInput from "./ChatInput";
-import Header from "./Header";
 import MessageList from "./MessageList";
-type Props = {
-  handleAside: () => void;
-};
-
-export default function ChatBox({ handleAside }: Props) {
+export default function ChatBox() {
   return (
-    <section className="flex flex-col items-center flex-1 h-full p-1 lg:border lg:rounded-lg">
-      <Header handleAside={handleAside} />
+    <section className="relative flex flex-col items-center flex-1 h-full p-1 lg:border lg:rounded-lg">
       <MessageList />
       <ChatInput />
     </section>
