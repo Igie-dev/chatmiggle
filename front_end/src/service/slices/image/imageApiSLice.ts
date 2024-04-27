e3f598fb90e0b962cc9c879d53f282caec86db07
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-27 23:35:33 +0800

    update

--- front_end/src/service/slices/image/imageApiSLice.ts
+++ front_end/src/service/slices/image/imageApiSLice.ts
@@ -2,36 +2,36 @@
 import { apiSlice } from "../api/apiSlice";
 
 const imageApiSlice = apiSlice.injectEndpoints({
-	endpoints: (builder) => ({
-		uploadAvatar: builder.mutation({
-			query: ({ data, id }: { data: FormData; id: string }) => ({
-				url: `/image/uploadavatar/${id}`,
-				method: "POST",
-				body: data,
-			}),
-			invalidatesTags: ["avatar"],
-		}),
+  endpoints: (builder) => ({
+    uploadAvatar: builder.mutation({
+      query: ({ data, id }: { data: FormData; id: string }) => ({
+        url: `/image/uploadavatar/${id}`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["avatar"],
+    }),
 
-		getAvatarLink: builder.query({
-			query: (id: string) => ({
-				url: `/image/avatar/${id}`,
-				method: "GET",
-			}),
-			providesTags: ["avatar"],
-		}),
+    getAvatarLink: builder.query({
+      query: (id: string) => ({
+        url: `/image/avatar/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["avatar"],
+    }),
 
-		deleteAvatar: builder.mutation({
-			query: (id: string) => ({
-				url: `/image/avatar/${id}`,
-				method: "DELETE",
-			}),
-			invalidatesTags: ["avatar"],
-		}),
-	}),
+    deleteAvatar: builder.mutation({
+      query: (id: string) => ({
+        url: `/image/avatar/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["avatar"],
+    }),
+  }),
 });
 
 export const {
-	useUploadAvatarMutation,
-	useGetAvatarLinkQuery,
-	useDeleteAvatarMutation,
+  useUploadAvatarMutation,
+  useGetAvatarLinkQuery,
+  useDeleteAvatarMutation,
 } = imageApiSlice;
