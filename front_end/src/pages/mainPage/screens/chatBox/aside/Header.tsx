5402f66322b970e448e76936fdb87c3977e9ef73
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-04-28 03:05:03 +0800

    Update

--- front_end/src/pages/mainPage/screens/chatBox/aside/Header.tsx
+++ front_end/src/pages/mainPage/screens/chatBox/aside/Header.tsx
@@ -1,27 +1,22 @@
 import { Button } from "@/components/ui/button";
-import { X } from "lucide-react";
+import { PencilLine, Image } from "lucide-react";
 import DisplayAvatar from "@/components/shared/DisplayAvatar";
 import { useAppSelector } from "@/service/store";
 import { getCurrentUser } from "@/service/slices/user/userSlice";
 import DisplayUserName from "@/components/shared/DisplayUserName";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Link } from "react-router-dom";
 type Props = {
-  handleAside: () => void;
   channel: TChannelData;
   isFetching: boolean;
 };
 
-function Header({ handleAside, channel, isFetching }: Props) {
+function Header({ channel, isFetching }: Props) {
   const { user_id } = useAppSelector(getCurrentUser);
   const mate = channel?.members.filter((m) => m.user_id !== user_id);
   return (
-    <header className="flex items-center justify-between w-full h-fit  px-5 flex-col pt-4  lg:w-[98%]">
-      <div className="flex items-center justify-end w-full xl:hidden">
-        <Button onClick={handleAside} size="icon" variant="ghost">
-          <X size={20} />
-        </Button>
-      </div>
-      <div className="flex items-center w-full gap-4 pb-8 mt-10 border-b">
+    <header className="flex flex-col items-center justify-between w-full gap-5 px-5 py-4 border rounded-md  h-fit bg-accent/50">
+      <div className="flex flex-col items-center justify-center w-full gap-2 ">
         <div className="w-16 h-16 overflow-hidden border rounded-full">
           {isFetching ? (
             <Skeleton className="w-full h-full" />
@@ -36,7 +31,7 @@ function Header({ handleAside, channel, isFetching }: Props) {
             <Skeleton className="w-full h-full" />
           </div>
         ) : (
-          <span className="text-medium font-normal  w-[70%] truncate">
+          <span className="font-normal truncate text-medium">
             {channel && channel?.is_private ? (
               <DisplayUserName userId={mate[0]?.user_id} />
             ) : (
@@ -45,6 +40,21 @@ function Header({ handleAside, channel, isFetching }: Props) {
           </span>
         )}
       </div>
+      {!channel?.is_private ? (
+        <div className="flex items-center justify-center w-full gap-1">
+          <Button size="icon" variant="outline">
+            <PencilLine size={20} />
+          </Button>
+          <Button size="icon" variant="outline">
+            <Link
+              to={`/avatar/upload/${channel?.channel_id}`}
+              className="flex !justify-start w-full !px-2"
+            >
+              <Image size={20} />
+            </Link>
+          </Button>
+        </div>
+      ) : null}
     </header>
   );
 }
