63f22e46f606af363b0f69c8945263d69fe61922
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-01 02:26:11 +0800

    fix

--- front_end/src/components/shared/SeenChannel.tsx
+++ front_end/src/components/shared/SeenChannel.tsx
@@ -12,23 +12,21 @@ export default function SeenChannel({ members, senderId, messageId }: Props) {
   const [isSeen, setIsSeen] = useState(false);
 
   useEffect(() => {
-    if (members.length >= 1) {
-      const usersSeen = members.filter(
-        (m) => m.user_id === user_id && m.is_seen && !m.is_deleted
-      );
-      setIsSeen(usersSeen.length >= 1);
-    }
+    const usersSeen = members?.filter(
+      (m) => m.user_id === user_id && m.is_seen && !m.is_deleted
+    );
+    setIsSeen(usersSeen.length >= 1);
   }, [user_id, members, messageId, senderId]);
 
   useEffect(() => {
     if (isSeen) return;
     socket.on("seen_channel", (res: { data: TChannelData }) => {
       if (res?.data) {
-        const foundUser = res?.data?.members.filter(
-          (m) => m.user_id === user_id && m.is_seen && !m.is_deleted
-        );
-        if (foundUser.length >= 1) {
-          setIsSeen(true);
+        if (!isSeen) {
+          const foundUser = res?.data?.members.filter(
+            (m) => m.user_id === user_id && m.is_seen && !m.is_deleted
+          );
+          setIsSeen(foundUser.length >= 1);
         }
       }
     });
@@ -36,7 +34,6 @@ export default function SeenChannel({ members, senderId, messageId }: Props) {
       socket.off("seen_channel");
     };
   }, [members, isSeen, user_id]);
-
   return senderId !== user_id ? (
     <>
       {isSeen ? null : (
--- front_end/src/components/shared/SeenMessage.tsx
+++ front_end/src/components/shared/SeenMessage.tsx
@@ -12,28 +12,33 @@ export default function SeenMessage({ message }: Props) {
 
   useEffect(() => {
     const members = message.channel.members;
-    if (members.length >= 1) {
-      const mates = message.channel?.members.filter(
-        (m) => m.is_seen && !m.is_deleted
-      );
-      if (membersSeen.length === 0) {
-        setMembersSeen(mates);
+    const matesSeen = members.filter((m) => m.is_seen && !m.is_deleted);
+    setMembersSeen(matesSeen);
+  }, [user_id, message]);
+
+  useEffect(() => {
+    const interval: NodeJS.Timeout = setInterval(async () => {
+      const checkUser = membersSeen.filter((m) => m.user_id === user_id);
+      if (checkUser.length >= 1) {
+        clearInterval(interval);
+        return;
       }
-    }
-  }, [user_id, message.channel.members, membersSeen.length]);
+      await asyncEmit("seen", {
+        channel_id: message.channel_id,
+        user_id,
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [membersSeen, message, user_id]);
 
   useEffect(() => {
     socket.on("message_seen", (res: { data: TChannelData }) => {
       if (res?.data) {
         const channel = res?.data;
-        if (channel.messages[0].message_id !== message.message_id) return;
-        const membersSeen = channel.members.filter(
-          (m) => m.is_seen && !m.is_deleted && m.user_id !== user_id
-        );
-
-        if (membersSeen.length !== membersSeen.length) {
-          setMembersSeen(membersSeen);
-        }
+        setMembersSeen(channel?.members);
       }
     });
 
@@ -41,36 +46,15 @@ export default function SeenMessage({ message }: Props) {
       socket.off("message_seen");
     };
   }, [user_id, message, message.sender_id]);
-
-  useEffect(() => {
-    const checkUserSeen = membersSeen.filter((m) => m.user_id === user_id);
-
-    if (checkUserSeen.length >= 1) return;
-    const interval: NodeJS.Timeout = setInterval(async () => {
-      await asyncEmit("seen", {
-        channel_id: message.channel_id,
-        user_id,
-      });
-      const checkAgain = membersSeen.filter((m) => m.user_id === user_id);
-      if (checkAgain.length === 0) {
-        clearInterval(interval);
-      }
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [message.channel_id, user_id, membersSeen]);
-
   return user_id === message.sender_id ? (
     <>
-      {membersSeen.length && message.channel.is_private ? (
+      {membersSeen.length > 1 && message?.channel?.is_private ? (
         <p className="mr-2  font-semibold text-[10px] opacity-50   rounded-lg">
           Seen
         </p>
       ) : null}
     </>
-  ) : !message.channel.is_private && membersSeen.length >= 1 ? (
+  ) : !message.channel.is_private && membersSeen.length > 1 ? (
     <DisplayGroupMemberSeen
       members={membersSeen}
       senderId={message.sender_id}
