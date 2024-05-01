eb771472ffa139bf6483a44203740c5455ead4b8
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-02 00:32:15 +0800

    remove

--- front_end/src/pages/mainPage/screens/channelScreen/ChannelScreen.tsx
+++ front_end/src/pages/mainPage/screens/channelScreen/ChannelScreen.tsx
@@ -1,7 +1,6 @@
 import { Outlet, useParams } from "react-router-dom";
 import AsideNav from "./aside/Aside";
 import Container from "../../../../components/shared/Container";
-import NoSelectedChannelUi from "@/components/shared/NoSelectedChannelUi";
 export default function ChannelScreen() {
   const { channelId } = useParams();
   return (
@@ -9,7 +8,7 @@ export default function ChannelScreen() {
       <>
         <AsideNav />
         <main className="w-full h-full lg:flex-1">
-          {!channelId ? <NoSelectedChannelUi /> : <Outlet />}
+          {!channelId ? null : <Outlet />}
         </main>
       </>
     </Container>
