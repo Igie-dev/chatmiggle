4b72aa50b9fd1bd7667147c27c1a14378f98ecc8
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-02 00:19:19 +0800

    update

--- front_end/src/service/slices/user/userSlice.ts
+++ front_end/src/service/slices/user/userSlice.ts
@@ -2,28 +2,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-	name: "user",
-	initialState: {
-		user_id: null,
-		email: null,
-		first_name: null,
-		last_name: null,
-	},
-	reducers: {
-		setCurrentUser: (state, action) => {
-			const { user_id, email, first_name, last_name } = action.payload;
-			state.email = email;
-			state.user_id = user_id;
-			state.first_name = first_name;
-			state.last_name = last_name;
-		},
-		removeCurrentUser: (state) => {
-			state.email = null;
-			state.user_id = null;
-			state.first_name = null;
-			state.last_name = null;
-		},
-	},
+  name: "user",
+  initialState: {
+    user_id: null,
+    email: null,
+    first_name: null,
+    last_name: null,
+  },
+  reducers: {
+    setCurrentUser: (state, action) => {
+      const { user_id, email, first_name, last_name } = action.payload;
+      state.email = email;
+      state.user_id = user_id;
+      state.first_name = first_name;
+      state.last_name = last_name;
+    },
+    removeCurrentUser: (state) => {
+      state.email = null;
+      state.user_id = null;
+      state.first_name = null;
+      state.last_name = null;
+    },
+  },
 });
 
 export const { setCurrentUser, removeCurrentUser } = userSlice.actions;
