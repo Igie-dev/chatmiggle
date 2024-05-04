30f438b1f0a0de00594e120cf4f14037603a215a
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-04 16:23:03 +0800

    Add change groupname listener

--- front_end/src/pages/mainPage/screens/chatBox/aside/Header.tsx
+++ front_end/src/pages/mainPage/screens/chatBox/aside/Header.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
-import { PencilLine, Image } from "lucide-react";
+import { Image } from "lucide-react";
 import DisplayAvatar from "@/components/shared/DisplayAvatar";
 import { useAppSelector } from "@/service/store";
 import { getCurrentUser } from "@/service/slices/user/userSlice";
 import DisplayUserName from "@/components/shared/DisplayUserName";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { socket } from "@/socket";
+import ChangeGroupName from "./ChangeGroupName";
 type Props = {
   channel: TChannelData;
   isFetching: boolean;
@@ -13,9 +16,23 @@ type Props = {
 
 function Header({ channel, isFetching }: Props) {
   const { user_id } = useAppSelector(getCurrentUser);
+  const [groupName, setGroupName] = useState("");
   const mate: TChannelMemberData[] = channel?.members.filter(
     (m) => m.user_id !== user_id
   );
+
+  useEffect(() => {
+    setGroupName(channel?.group_name as string);
+    socket.on("new_group_name", (res) => {
+      if (res?.data) {
+        const groupName = res?.data?.group_name;
+        if (groupName) {
+          setGroupName(groupName);
+        }
+      }
+    });
+  }, [channel?.group_name]);
+
   return (
     <header className="flex flex-col items-center justify-between w-full gap-5 px-5 py-4 border rounded-md h-fit bg-accent/50">
       <div className="flex flex-col items-center justify-center w-full gap-2 ">
@@ -37,16 +54,18 @@ function Header({ channel, isFetching }: Props) {
             {channel && channel?.is_private ? (
               <DisplayUserName userId={mate[0]?.user_id} />
             ) : (
-              channel?.group_name
+              groupName
             )}
           </span>
         )}
       </div>
       {!channel?.is_private ? (
         <div className="flex items-center justify-center w-full gap-1">
-          <Button size="icon" variant="outline">
-            <PencilLine size={20} />
-          </Button>
+          <ChangeGroupName
+            channelId={channel?.channel_id}
+            groupName={channel?.group_name as string}
+            avatarId={channel?.avatar_id as string}
+          />
           <Button size="icon" variant="outline">
             <Link
               to={`/avatar/upload/${channel?.channel_id}`}
