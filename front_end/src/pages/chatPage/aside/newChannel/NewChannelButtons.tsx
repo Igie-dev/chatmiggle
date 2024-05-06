62eb10a21c9371071d99d0af1c72cda914375b24
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-06 23:17:02 +0800

    Change folder

--- front_end/src/pages/chatPage/aside/newChannel/NewChannelButtons.tsx
+++ front_end/src/pages/chatPage/aside/newChannel/NewChannelButtons.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "../../../../../components/ui/button";
+import { Button } from "@/components/ui/button";
 import CreateNewGroup from "./CreateNewGroup";
 import CreateNewChannel from "./CreateNewChannel";
 import { Plus } from "lucide-react";
