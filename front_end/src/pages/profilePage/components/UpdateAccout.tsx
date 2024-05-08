c7856de74c505fbb4ac6adab77e8d13919cf95b9
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-08 21:45:33 +0800

    Update account

--- front_end/src/pages/profilePage/components/UpdateAccout.tsx
+++ front_end/src/pages/profilePage/components/UpdateAccout.tsx
@@ -13,50 +13,63 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { useUpdateUserMutation } from "@/service/slices/user/userApiSlice";
+import BtnsLoaderSpinner from "@/components/loader/BtnLoader";
 type Props = {
   user: TUser;
 };
 export default function UpdateAccout({ user }: Props) {
   const [open, setOpen] = useState(false);
-
+  const [update, { isLoading, error }] = useUpdateUserMutation();
   const formik = useFormik({
     initialValues: {
       firstName: "",
       lastName: "",
-      email: "",
     },
     validationSchema: new Yup.ObjectSchema({
-      firstName: Yup.string().required("Please provide your First Name"),
-      lastName: Yup.string().required("Please provide your Last Name"),
-      email: Yup.string().required("Email is required").email(),
+      firstName: Yup.string().required("Please provide your first name"),
+      lastName: Yup.string().required("Please provide your last name"),
     }),
     onSubmit: async (values) => {
-      console.log(values);
-
       if (
         user?.first_name === values.firstName &&
-        user?.last_name === values.lastName &&
-        user?.email === values.email
+        user?.last_name === values.lastName
       )
         return;
+
+      const data = {
+        user_id: user.user_id,
+        first_name: values.firstName,
+        last_name: values.lastName,
+      };
+
+      try {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const res: any = await update(data);
+        if (res?.data) {
+          setOpen(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
 
   useLayoutEffect(() => {
-    if (user) {
-      if (
-        formik.values.firstName ||
-        formik.values.lastName ||
-        formik.values.email
-      )
-        return;
-      formik.setValues({
-        firstName: user.first_name,
-        lastName: user.last_name,
-        email: user.email,
-      });
+    if (open) {
+      if (!formik.values.firstName && !formik.values.lastName) {
+        formik.setValues({
+          firstName: user.first_name,
+          lastName: user.last_name,
+        });
+      }
+    } else {
+      if (formik.values.firstName && formik.values.lastName) {
+        formik.resetForm();
+      }
     }
-  }, [user, formik]);
+  }, [user, open, formik]);
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
@@ -64,39 +77,79 @@ export default function UpdateAccout({ user }: Props) {
           Update profile
         </Button>
       </DialogTrigger>
-      <form onSubmit={formik.handleSubmit}>
-        <DialogContent className="sm:max-w-[30rem]">
+
+      <DialogContent className="sm:max-w-[30rem]">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex flex-col w-full gap-5"
+        >
           <DialogHeader>
             <DialogTitle>Edit profile</DialogTitle>
             <DialogDescription>
               Make changes to your profile here. Click save when you're done.
             </DialogDescription>
           </DialogHeader>
-          <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-2">
+          <p>{error?.data?.error}</p>
+          <div className="flex flex-col gap-6">
+            <div className="relative flex flex-col gap-1">
               <Label htmlFor="firstName">First Name</Label>
               <Input
-                defaultValue={formik.values.firstName}
+                id="firstName"
+                value={formik.values.firstName}
                 placeholder="First Name"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                disabled={isLoading}
+                className={`${
+                  formik.touched.firstName && formik.errors.firstName
+                    ? "border-destructive"
+                    : "border-border"
+                }`}
               />
+              <p className="absolute left-0 text-xs -bottom-5 text-destructive">
+                {formik.touched.firstName && formik.errors.firstName
+                  ? formik.errors.firstName
+                  : null}
+              </p>
             </div>
-            <div className="flex flex-col gap-2">
+            <div className="relative flex flex-col gap-1">
               <Label htmlFor="lastName">Last Name</Label>
               <Input
-                defaultValue={formik.values.lastName}
+                id="lastName"
+                value={formik.values.lastName}
                 placeholder="Last Name"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                disabled={isLoading}
+                className={`${
+                  formik.touched.lastName && formik.errors.lastName
+                    ? "border-destructive"
+                    : "border-border"
+                }`}
               />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input defaultValue={formik.values.email} placeholder="Email" />
+              <p className="absolute left-0 text-xs -bottom-5 text-destructive">
+                {formik.touched.lastName && formik.errors.lastName
+                  ? formik.errors.lastName
+                  : null}
+              </p>
             </div>
           </div>
           <DialogFooter>
-            <Button type="submit">Save changes</Button>
+            <Button
+              type="submit"
+              disabled={
+                (user?.first_name === formik.values.firstName &&
+                  user?.last_name === formik.values.lastName) ||
+                !formik.values.lastName ||
+                !formik.values.firstName ||
+                isLoading
+              }
+            >
+              {isLoading ? <BtnsLoaderSpinner /> : "Save changes"}
+            </Button>
           </DialogFooter>
-        </DialogContent>
-      </form>
+        </form>
+      </DialogContent>
     </Dialog>
   );
 }
