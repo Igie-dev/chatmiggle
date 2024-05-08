7c765bd734e4a829e82ebaa0539339d07faecfb4
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-08 21:51:07 +0800

    Display User if user not found

--- front_end/src/components/shared/DisplayUserName.tsx
+++ front_end/src/components/shared/DisplayUserName.tsx
@@ -9,8 +9,10 @@ export default function DisplayUserName({ userId }: Props) {
     <>
       {isFetching ? (
         <Skeleton className="w-full h-full" />
-      ) : (
+      ) : data?.first_name && data?.last_name ? (
         <>{`${data?.first_name} ${data?.last_name}`}</>
+      ) : (
+        <>User</>
       )}
     </>
   );
