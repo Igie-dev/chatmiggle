f86eede935323f6cfa3e26ec670f67560d4484e0
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-08 21:41:12 +0800

    update delete account

--- front_end/src/pages/profilePage/components/DeleteAccount.tsx
+++ front_end/src/pages/profilePage/components/DeleteAccount.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { useDeleteUserByIdMutation } from "@/service/slices/user/userApiSlice";
 import {
   Dialog,
   DialogContent,
@@ -9,17 +10,35 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import BtnsLoaderSpinner from "@/components/loader/BtnLoader";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   userId: string;
 };
 export default function DeleteAccount({ userId }: Props) {
   const [open, setOpen] = useState(false);
+  const [deletAccount, { isLoading }] = useDeleteUserByIdMutation();
+  const navigate = useNavigate();
+  const handleClick = async () => {
+    if (!userId) return;
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const res: any = await deletAccount(userId);
+      console.log(res);
+      if (res?.data) {
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button variant="destructive" size="sm">
-          Remove account
+          Delete account
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
@@ -32,7 +51,9 @@ export default function DeleteAccount({ userId }: Props) {
         </DialogHeader>
 
         <DialogFooter>
-          <Button type="submit">Delete account</Button>
+          <Button type="button" onClick={handleClick}>
+            {isLoading ? <BtnsLoaderSpinner /> : "Delete account"}
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
