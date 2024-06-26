dc2d01430a5dc36a7093b51fa4b7dc95cd5ef69e
igie <baldesansoigieboysanchez@gmail.com>
2024-06-26 13:41:52 +0800

    listed add new group member

--- front_end/src/hooks/useListenAddGroupMember.ts
+++ front_end/src/hooks/useListenAddGroupMember.ts
@@ -1,17 +1,22 @@
 import { useEffect, useState } from 'react'
 import { socket } from "@/socket";
-export default function useListenAddGroupMember(channelId: string) {
+import { useParams } from 'react-router-dom';
+export default function useListenAddGroupMember() {
     const [newMembers, setNewMembers] = useState<TChannelMemberData[] | null>(null);
-
+    const { channelId } = useParams();
     useEffect(() => {
         socket.on(
             "add_member",
             (res: { data: TChannelData }) => {
                 const channel_Id = res.data.channel_id;
                 const members = res.data.members
-                if (channel_Id !== channelId) return;
-                const filterMembers = members.filter((member) => !member.is_deleted);
-                setNewMembers(filterMembers);
+                if (channel_Id !== channelId) {
+                    setNewMembers(null)
+                } else {
+                    const filterMembers = members.filter((member) => !member.is_deleted);
+                    setNewMembers(filterMembers);
+                }
+
             }
         );
 
