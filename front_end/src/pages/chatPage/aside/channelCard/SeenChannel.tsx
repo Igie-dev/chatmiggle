dc0490df1aed4e31a2895e404dbeba68d5198468
igie <baldesansoigieboysanchez@gmail.com>
2024-06-26 13:45:44 +0800

    fix seen endicator

--- front_end/src/pages/chatPage/aside/channelCard/SeenChannel.tsx
+++ front_end/src/pages/chatPage/aside/channelCard/SeenChannel.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { useAppSelector } from "@/service/store";
 import { getCurrentUser } from "@/service/slices/user/userSlice";
 import useListenMessageSeen from "@/hooks/useListenMessageSeen";
@@ -9,32 +9,25 @@ type Props = {
 };
 export default function SeenChannel({ members, senderId, messageId }: Props) {
   const { user_id } = useAppSelector(getCurrentUser);
-  const [isSeen, setIsSeen] = useState(false);
   const seenChannel = useListenMessageSeen();
+  const isSeen = useMemo((): boolean => {
+    let seen = false;
 
-  useEffect(() => {
-    const isUserSeen = members.filter(
-      (m) => m.is_seen && !m.is_deleted && m.user_id === user_id
-    );
-    if (isUserSeen.length >= 1) {
-      setIsSeen(true);
-    } else {
-      setIsSeen(false);
-    }
-  }, [members, user_id]);
-
-  useEffect(() => {
-    if (isSeen) return;
-    if (seenChannel) {
-      if (seenChannel.messages[0].message_id !== messageId) return;
-      const foundUser = seenChannel.members.filter(
-        (m) => m.user_id === user_id && m.is_seen && !m.is_deleted
+    if (members?.length >= 1) {
+      const membersSeen = members.filter(
+        (m) => m.is_seen && !m.is_deleted && m.user_id === user_id
       );
-      if (foundUser.length >= 1) {
-        setIsSeen(true);
-      }
+      seen = membersSeen.length >= 1;
     }
-  }, [isSeen, user_id, seenChannel, messageId]);
+
+    if (!seen && seenChannel?.messages[0].message_id === messageId) {
+      const membersSeen = seenChannel.members.filter(
+        (m) => m.is_seen && !m.is_deleted && m.user_id === user_id
+      );
+      seen = membersSeen.length >= 1;
+    }
+    return seen;
+  }, [messageId, members, seenChannel, user_id]);
 
   if (senderId !== user_id && !isSeen) {
     return <p className="absolute top-1 right-2 text-[10px] opacity-50">New</p>;
