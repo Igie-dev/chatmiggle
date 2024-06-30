67c586751fc10f80657f7b7af5ca1a83204c57d1
igie <baldesansoigieboysanchez@gmail.com>
2024-07-01 00:44:33 +0800

    update buttons

--- front_end/src/pages/chatPage/aside/newChannel/NewChannelButtons.tsx
+++ front_end/src/pages/chatPage/aside/newChannel/NewChannelButtons.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import CreateNewGroup from "./CreateNewGroup";
-import CreateNewChannel from "./CreateNewChannel";
+import CreateChannel from "./CreateChannel";
+import RequestJoinChannel from "./RequestJoinChannel";
 import { Plus } from "lucide-react";
 
 export default function NewChannelButtons() {
@@ -10,22 +10,7 @@ export default function NewChannelButtons() {
   return (
     <div className="absolute flex flex-col items-end gap-2 pr-5 w-fit h-fit bottom-4 right-2">
       <>
-        <CreateNewChannel>
-          <Button
-            variant="secondary"
-            size="icon"
-            title="New chat"
-            onClick={() => setIsShow(false)}
-            className={`w-full p-2 transition-all translate-y-10  border rounded-md opacity-0 h-fit group bg-secondary/50 ${
-              isShow
-                ? "-translate-y-5  opacity-50 hover:opacity-100 hover:bg-secondary hover:scale-105  delay-100 "
-                : "translate-y-10 pointer-events-none  delay-200 "
-            }`}
-          >
-            <p className="text-xs font-light ">New Chat</p>
-          </Button>
-        </CreateNewChannel>
-        <CreateNewGroup>
+        <CreateChannel>
           <Button
             variant="secondary"
             title="New chat"
@@ -33,13 +18,28 @@ export default function NewChannelButtons() {
             onClick={() => setIsShow(false)}
             className={`w-full p-2 transition-all translate-y-10  border rounded-md opacity-0 h-fit group bg-secondary/50 ${
               isShow
-                ? "-translate-y-5 opacity-50 hover:opacity-100 hover:bg-secondary hover:scale-105 delay-200 "
+                ? "-translate-y-10 opacity-80 hover:opacity-100 hover:bg-secondary hover:scale-105 delay-210 "
                 : "translate-y-10 pointer-events-none  delay-100"
             }`}
           >
-            <p className="text-xs font-light ">New Group</p>
+            <p className="text-xs font-light ">Create channel</p>
           </Button>
-        </CreateNewGroup>
+        </CreateChannel>
+        <RequestJoinChannel>
+          <Button
+            variant="secondary"
+            title="New chat"
+            size="icon"
+            onClick={() => setIsShow(false)}
+            className={`w-full p-2 transition-all translate-y-10  border rounded-md opacity-0 h-fit group bg-secondary/50 ${
+              isShow
+                ? "-translate-y-9 opacity-80 hover:opacity-100 hover:bg-secondary hover:scale-105 delay-200"
+                : "translate-y-10 pointer-events-none  delay-110"
+            }`}
+          >
+            <p className="text-xs font-light ">Join channel</p>
+          </Button>
+        </RequestJoinChannel>
       </>
 
       <Button
