1710f206e31dbd048d5365e9930b1a753462acae
igie <baldesansoigieboysanchez@gmail.com>
2024-07-01 00:34:40 +0800

    add more channel info

--- front_end/src/service/slices/channel/channelSlice.ts
+++ front_end/src/service/slices/channel/channelSlice.ts
@@ -4,36 +4,40 @@ import { createSlice } from "@reduxjs/toolkit";
 const channelSlice = createSlice({
 	name: "channel",
 	initialState: {
-		channelId: null,
+		channelId: "",
+		adminId: "",
 		members: [],
+		avatarId: "",
+		channelName: ""
 	},
 	reducers: {
-		setCurrentChannelId: (state, action) => {
-			const channelId = action.payload;
+		setCurrentChannel: (state, action) => {
+			const { channelId, members, adminId, avatarId, channelName } = action.payload;
 			state.channelId = channelId;
-		},
-
-		setCurrentChannelMembers: (state, action) => {
-			const members = action.payload;
+			state.adminId = adminId;
 			state.members = members;
+			state.avatarId = avatarId;
+			state.channelName = channelName
 		},
 
-		removeCurrentChannelId: (state) => {
-			state.channelId = null;
-		},
-		removeCurrentChannelMembers: (state) => {
+		removeCurrentChannel: (state) => {
+			state.adminId = "";
+			state.channelId = "";
 			state.members = [];
-		},
+			state.avatarId = "";
+			state.channelName = "";
+		}
 	},
 });
 
 export const {
-	setCurrentChannelId,
-	setCurrentChannelMembers,
-	removeCurrentChannelId,
-	removeCurrentChannelMembers,
+	setCurrentChannel,
+	removeCurrentChannel
 } = channelSlice.actions;
 
 export const getCurrentChannelId = (state: any) => state.channel.channelId;
-export const getCurrentChanneMember = (state: any) => state.channel.members;
+export const getCurrentChanneMembers = (state: any) => state.channel.members;
+export const getCurrentChannelAdminId = (state: any) => state.channel.adminId;
+export const getCurrentChannelAvatar = (state: any) => state.channel.avatarId;
+export const getCurrentChannelName = (state: any) => state.channel.channelName;
 export default channelSlice.reducer;
