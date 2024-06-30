00626ce9709b2273120ffe010c25f87f9b3cc8a7
igie <baldesansoigieboysanchez@gmail.com>
2024-07-01 00:45:26 +0800

    add set channel state

--- front_end/src/pages/chatPage/screens/chatBox/ChatBoxContainer.tsx
+++ front_end/src/pages/chatPage/screens/chatBox/ChatBoxContainer.tsx
@@ -1,6 +1,37 @@
+import LoaderSpinner from "@/components/loader/LoaderSpinner";
 import ChatboxAside from "./aside/ChatboxAside";
 import ChatBox from "./chatbox/ChatBox";
+import { useGetChannelQuery } from "@/service/slices/channel/channelApiSlice";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useAppDispatch } from "@/service/store";
+import { setCurrentChannel } from "@/service/slices/channel/channelSlice";
 export default function ChatBoxContainer() {
+  const { channelId } = useParams();
+  const { data, isFetching } = useGetChannelQuery(channelId as string);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (data?.channelId) {
+      const dataChannelId = data?.channelId;
+      const admin = data?.members?.filter(
+        (member: TChannelMemberData) => member.isAdmin
+      );
+      const members = data?.members;
+      dispatch(
+        setCurrentChannel({
+          channelId: dataChannelId,
+          adminId: admin[0]?.userId ?? "",
+          members,
+          avatarId: data?.avatarId ?? "",
+          channelName: data?.channelName ?? "",
+        })
+      );
+    }
+  }, [data, dispatch]);
+
+  if (isFetching) return <LoaderSpinner />;
+
   return (
     <section className="relative flex items-center w-full h-full gap-2">
       <div className="relative flex items-center w-full h-full overflow-hidden">
