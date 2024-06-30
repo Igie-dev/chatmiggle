aa65e818860390f2d0c7c5099e84e3414abd6af2
igie <baldesansoigieboysanchez@gmail.com>
2024-07-01 00:42:12 +0800

    update header

--- front_end/src/pages/chatPage/screens/chatBox/aside/Header.tsx
+++ front_end/src/pages/chatPage/screens/chatBox/aside/Header.tsx
@@ -1,60 +1,65 @@
 import { Button } from "@/components/ui/button";
-import { Image, Trash } from "lucide-react";
+import { Share, Image, Trash, Copy } from "lucide-react";
 import DisplayAvatar from "@/components/shared/DisplayAvatar";
 import { useAppSelector } from "@/service/store";
 import { getCurrentUser } from "@/service/slices/user/userSlice";
-import DisplayUserName from "@/components/shared/DisplayUserName";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useLayoutEffect, useState } from "react";
 import ChangeGroupName from "./ChangeGroupName";
-import AddMember from "./AddMember";
-import LeaveGroup from "./LeaveGroup";
+import RemoveFromChannel from "./RemoveFromChannel";
 import DeleteChannel from "./DeleteChannel";
 import CustomTooltip from "@/components/shared/CustomTooltip";
 import useListenChangeGroupName from "@/hooks/useListenChangeGroupName";
-type Props = {
-  channel: TChannelData;
-  isFetching: boolean;
-};
-
-function Header({ channel, isFetching }: Props) {
-  const { user_id } = useAppSelector(getCurrentUser);
+import { useGetChannelQuery } from "@/service/slices/channel/channelApiSlice";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+function Header() {
+  const { userId: currentUserId } = useAppSelector(getCurrentUser);
   const [groupName, setGroupName] = useState("");
   const [adminId, setAdminId] = useState("");
-  const newGroupName = useListenChangeGroupName(channel.channel_id);
-  const mate: TChannelMemberData[] = channel?.members.filter(
-    (m) => m.user_id !== user_id
-  );
+  const { channelId } = useParams();
+  const { data, isFetching } = useGetChannelQuery(channelId as string);
+  const newGroupName = useListenChangeGroupName(channelId as string);
 
-  const isUserAMember = channel?.members.filter((m) => m.user_id === user_id);
   useLayoutEffect(() => {
-    setGroupName(channel?.group_name as string);
+    setGroupName(data?.channelName as string);
     if (newGroupName) {
       setGroupName(newGroupName);
     }
-  }, [channel?.group_name, newGroupName]);
+  }, [data, newGroupName]);
 
   useLayoutEffect(() => {
-    if (!channel?.channel_id) return;
-    const getAdmin = channel?.members.filter(
-      (m) => !m.is_deleted && m.is_admin
+    if (!data?.channelId) return;
+    const getAdmin = data?.members.filter(
+      (m: TChannelMemberData) => m.joinApproved && m.isAdmin
     );
     if (getAdmin.length >= 1) {
-      setAdminId(getAdmin[0].user_id);
+      setAdminId(getAdmin[0].userId);
     }
-  }, [channel]);
+  }, [data]);
 
+  const handleCopyId = () => {
+    navigator.clipboard.writeText(`${data?.channelId}`);
+  };
   return (
     <header className="flex flex-col items-center justify-between w-full gap-5 px-5 py-4 h-fit">
       <div className="flex flex-col items-center justify-center w-full space-y-2">
         <div className="overflow-hidden border rounded-full w-14 h-14">
           {isFetching ? (
             <Skeleton className="w-full h-full" />
-          ) : channel && channel?.is_private ? (
-            <DisplayAvatar id={(mate[0]?.user?.avatar_id as string) ?? ""} />
           ) : (
-            <DisplayAvatar id={(channel?.avatar_id as string) ?? ""} />
+            <DisplayAvatar id={(data?.avatarId as string) ?? ""} />
           )}
         </div>
         {isFetching ? (
@@ -62,80 +67,87 @@ function Header({ channel, isFetching }: Props) {
             <Skeleton className="w-full h-full" />
           </div>
         ) : (
-          <span className="text-sm font-semibold truncate">
-            {channel && channel?.is_private ? (
-              <DisplayUserName userId={mate[0]?.user_id} />
-            ) : (
-              groupName
-            )}
-          </span>
+          <span className="text-sm font-semibold truncate">{groupName}</span>
         )}
       </div>
-
       <div className="flex items-center justify-center w-full gap-1 ">
-        {adminId === user_id && !channel?.is_private ? (
-          <AddMember
-            channelId={channel?.channel_id}
-            groupName={channel?.group_name as string}
-            channelAvatarId={channel?.avatar_id as string}
-          />
-        ) : (
-          <span />
-        )}
-        {!channel?.is_private ? (
-          <ChangeGroupName
-            channelId={channel?.channel_id}
-            groupName={channel?.group_name as string}
-            avatarId={channel?.avatar_id as string}
-          />
-        ) : null}
-        {!channel?.is_private ? (
-          <Button size="icon" variant="outline">
-            <Link
-              to={`/avatar/upload/${channel?.channel_id}`}
-              className="flex !justify-center w-full !px-2"
-            >
-              <CustomTooltip title="Change profile">
-                <Image size={20} />
-              </CustomTooltip>
-            </Link>
-          </Button>
-        ) : null}
-        {adminId !== user_id && !channel?.is_private ? (
-          <LeaveGroup
-            userId={user_id}
-            channelId={channel?.channel_id}
-            groupName={channel?.group_name as string}
+        <ChangeGroupName
+          channelId={data?.channelId}
+          groupName={data?.channelName as string}
+          avatarId={data?.avatarId as string}
+        />
+
+        <Button size="icon" variant="outline">
+          <Link
+            to={`/avatar/upload/${data?.channelId}`}
+            className="flex !justify-center w-full !px-2"
+          >
+            <CustomTooltip title="Change profile">
+              <Image size={20} />
+            </CustomTooltip>
+          </Link>
+        </Button>
+
+        {adminId !== currentUserId ? (
+          <RemoveFromChannel
+            userId={currentUserId}
+            channelId={data?.channelId}
+            channelName={data?.channelName as string}
             cardDescription="Are you sure you want to leave this group?"
             cardTitle="Leave Group"
             type="leave"
-            channelAvatarId={channel?.avatar_id as string}
+            channelAvatarId={data?.avatarId as string}
           >
             <Button size="icon" variant="outline">
               <CustomTooltip title="Leave group">
                 <Trash size={20} />
               </CustomTooltip>
             </Button>
-          </LeaveGroup>
+          </RemoveFromChannel>
         ) : null}
-        {adminId === user_id && !channel?.is_private ? (
+        {adminId === currentUserId ? (
           <DeleteChannel
-            channelId={channel?.channel_id}
-            groupName={channel?.group_name as string}
-            userId={channel?.is_private ? user_id : adminId}
-            isPrivate={channel?.is_private}
-            channelAvatarId={channel?.avatar_id as string}
+            channelId={data?.channelId}
+            groupName={data?.channelName as string}
+            channelAvatarId={data?.avatarId as string}
+            userId={currentUserId}
           />
         ) : null}
-        {isUserAMember?.length >= 1 && channel?.is_private ? (
-          <DeleteChannel
-            channelId={channel?.channel_id}
-            groupName={channel?.group_name as string}
-            userId={channel?.is_private ? user_id : adminId}
-            isPrivate={channel?.is_private}
-            channelAvatarId={channel?.avatar_id as string}
-          />
-        ) : null}
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon" variant="outline">
+              <Share size={20} />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-md">
+            <DialogHeader>
+              <DialogTitle>Copy ID</DialogTitle>
+              <DialogDescription>
+                Copy your ID and share it with whoever you want to send you a
+                message.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex items-center space-x-2">
+              <div className="grid flex-1 gap-2">
+                <Label htmlFor="channelId" className="sr-only">
+                  ID
+                </Label>
+                <Input id="channelId" defaultValue={channelId} readOnly />
+              </div>
+              <Button size="sm" onClick={handleCopyId} className="px-3">
+                <span className="sr-only">Copy</span>
+                <Copy className="w-4 h-4" />
+              </Button>
+            </div>
+            <DialogFooter className="sm:justify-start">
+              <DialogClose asChild>
+                <Button type="button" variant="secondary">
+                  Close
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </header>
   );
