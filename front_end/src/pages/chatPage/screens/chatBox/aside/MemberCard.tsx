9c4bb7088dd301655bc0cfc92722ae12d326164c
igie <baldesansoigieboysanchez@gmail.com>
2024-07-01 00:42:00 +0800

    Update member card

--- front_end/src/pages/chatPage/screens/chatBox/aside/MemberCard.tsx
+++ front_end/src/pages/chatPage/screens/chatBox/aside/MemberCard.tsx
@@ -1,86 +1,50 @@
 import DisplayAvatar from "@/components/shared/DisplayAvatar";
 import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useGetUserByIdQuery } from "@/service/slices/user/userApiSlice";
 import { User, X } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "@/service/store";
 import { getCurrentUser } from "@/service/slices/user/userSlice";
-import LeaveGroup from "./LeaveGroup";
-import { useNavigate } from "react-router-dom";
+import { getCurrentChannelAdminId } from "@/service/slices/channel/channelSlice";
+import RemoveFromChannel from "./RemoveFromChannel";
 type Props = {
-  userId: string;
-  channelId: string;
-  channelAvatarId: string;
-  groupName: string;
-  isPrivate: boolean;
-  adminId: string;
+  user: TUserData;
 };
-export default function MemberCard({
-  userId,
-  channelId,
-  groupName,
-  isPrivate,
-  adminId,
-  channelAvatarId,
-}: Props) {
-  const { user_id } = useAppSelector(getCurrentUser);
-  const { data, isFetching, isError } = useGetUserByIdQuery(userId);
+export default function MemberCard({ user }: Props) {
   const navigate = useNavigate();
-
-  if (isError) return null;
-  return isFetching ? (
-    <li className="flex items-center w-full gap-3 p-2 border rounded-md cursor-pointer h-fit">
-      <div className="overflow-hidden rounded-full w-11 h-11">
-        <Skeleton className="w-full h-full" />
-      </div>
-      <div className="flex flex-col w-[80%] h-full justify-center gap-2">
-        <div className="w-full h-3">
-          <Skeleton className="w-full h-full" />
-        </div>
-      </div>
-    </li>
-  ) : (
-    <li
-      className={`group relative flex items-center w-full justify-between gap-1 p-2 bg-transparent  rounded-md cursor-pointer h-fit  hover:bg-secondary/50 ${
-        isFetching ? "hover:cursor-wait" : "hover:cursor-pointer"
-      }`}
-    >
+  const { userId } = useAppSelector(getCurrentUser);
+  const adminId = useAppSelector(getCurrentChannelAdminId);
+  return (
+    <li className="relative flex items-center justify-between w-full gap-1 p-2 bg-transparent rounded-md cursor-pointer group h-fit hover:bg-secondary/50">
       <div className="flex items-center flex-1 h-full gap-2">
         <div className="w-8 h-8">
-          <DisplayAvatar id={data?.avatar_id as string} />
+          <DisplayAvatar id={user?.avatarId as string} />
         </div>
         <div className="items-start w-[75%]  flex flex-col">
           <p className="w-full max-w-full text-xs truncate max-h-6">
-            {data?.first_name + " " + data?.last_name}
+            {user?.firstName + " " + user?.lastName}
           </p>
           <p className="text-muted-foreground text-[10px]">
-            {adminId === userId && !isPrivate ? "Admin" : ""}
+            {adminId === user.userId ? "Admin" : ""}
           </p>
         </div>
       </div>
-      {data?.user_id !== user_id ? (
+      {user?.userId !== userId ? (
         <Button
           size="icon"
           title="Profile"
           variant="ghost"
-          onClick={() => navigate(`/profile/${data?.user_id}`)}
+          onClick={() => navigate(`/profile/${user?.userId}`)}
           className="opacity-50 hover:opacity-100"
         >
           <User size={20} />
         </Button>
       ) : null}
-      {adminId === user_id && adminId !== userId ? (
-        <LeaveGroup
-          userId={userId}
-          channelId={channelId}
-          groupName={groupName}
+      {userId === adminId && user?.userId !== userId ? (
+        <RemoveFromChannel
           cardDescription=" Are you sure you want to remove this user?"
           cardTitle="Remove user"
-          firstName={data?.first_name}
-          lastName={data?.last_name}
-          channelAvatarId={channelAvatarId}
+          user={user}
           type="remove"
-          userAvatarId={data?.avatar_id as string}
         >
           <Button
             size="icon"
@@ -90,7 +54,7 @@ export default function MemberCard({
           >
             <X size={20} />
           </Button>
-        </LeaveGroup>
+        </RemoveFromChannel>
       ) : null}
     </li>
   );
