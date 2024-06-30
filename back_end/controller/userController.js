cb90dd330f1779f49c5f2a7ec63ee8dbc6e9d408
igie <baldesansoigieboysanchez@gmail.com>
2024-07-01 00:31:28 +0800

    update user controller

--- back_end/controller/userController.js
+++ back_end/controller/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "express-async-handler";
 import prisma from "../utils/prisma.js";
-
+import { userDto, usersDto } from "../dto/userDto.js";
 const getUsers = asyncHandler(async (req, res) => {
   const cursor = Number(req.query.cursor);
   const take = req.query.take;
@@ -35,18 +35,18 @@ const getUsers = asyncHandler(async (req, res) => {
       return res.status(404).json({ error: "No users found" });
     }
     const newCursor = users[users?.length - 1]?.id;
-    return res.status(200).json({ cursor: newCursor, users: users });
+    return res.status(200).json({ cursor: newCursor, users: usersDto(users) });
   } catch (error) {
     return res.status(500).json({ error: "Something went wrong" });
   }
 });
 
 const getUser = asyncHandler(async (req, res) => {
-  const user_id = req.params.id;
+  const userId = req.params.id;
 
   try {
     const foundUser = await prisma.user.findUnique({
-      where: { user_id },
+      where: { user_id: userId },
       select: {
         id: true,
         user_id: true,
@@ -63,21 +63,21 @@ const getUser = asyncHandler(async (req, res) => {
       return res.status(404).json({ error: "User not found" });
     }
 
-    return res.status(200).json(foundUser);
+    return res.status(200).json(userDto(foundUser));
   } catch (error) {
     return res.status(500).json({ error: "Something went wrong" });
   }
 });
 
 const updateUser = asyncHandler(async (req, res) => {
-  const { user_id, first_name, last_name } = req.body;
-  if (!user_id || !first_name || !last_name) {
+  const { userId, firstName, lastName } = req.body;
+  if (!userId || !firstName || !lastName) {
     return res.status(400).json({ error: "All field are required" });
   }
   try {
     const update = await prisma.user.update({
-      where: { user_id },
-      data: { first_name, last_name },
+      where: { user_id: userId },
+      data: { first_name: firstName, last_name: lastName },
     });
 
     if (!update?.id) {
@@ -90,16 +90,18 @@ const updateUser = asyncHandler(async (req, res) => {
 });
 
 const deleteUser = asyncHandler(async (req, res) => {
-  const user_id = req.params.id;
+  const userId = req.params.id;
 
   try {
-    const foundUser = await prisma.user.findUnique({ where: { user_id } });
+    const foundUser = await prisma.user.findUnique({
+      where: { user_id: userId },
+    });
 
     if (!foundUser?.id) {
       return res.status(404).json({ error: "User not found" });
     }
 
-    const deletedUser = await prisma.user.delete({ where: { user_id } });
+    const deletedUser = await prisma.user.delete({ where: { userId } });
 
     if (!deletedUser?.id) {
       return res.status(500).json({ error: "Something went wrong" });
@@ -110,78 +112,4 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
-const getUserFriends = asyncHandler(async (req, res) => {
-  const user_id = req.params.id;
-  try {
-    const foundUser = await prisma.user.findUnique({
-      where: { user_id },
-      include: {
-        membered_channel: {
-          include: {
-            channel: true,
-          },
-        },
-      },
-    });
-
-    if (!foundUser?.id || foundUser?.membered_channel?.length <= 0) {
-      return res.status(404).json({ error: "No friends found" });
-    }
-    const userChannels = [];
-    for await (const channel of foundUser?.membered_channel) {
-      const existChannel = userChannels.filter(
-        (c) => c.channel_id === channel.channel_id
-      );
-      if (existChannel.length <= 0) {
-        const foundChannel = await prisma.channel.findUnique({
-          where: { channel_id: channel.channel_id, is_private: true },
-          include: {
-            messages: {
-              orderBy: {
-                createdAt: "desc",
-              },
-              take: 1,
-              include: {
-                channel: {
-                  include: {
-                    members: true,
-                  },
-                },
-              },
-            },
-            members: true,
-          },
-        });
-
-        if (foundChannel?.messages?.length >= 1) {
-          userChannels.push(foundChannel);
-        }
-      }
-    }
-
-    if (userChannels?.length <= 0) {
-      return res.status(404).json({ error: "No friends found" });
-    }
-
-    userChannels?.sort(
-      (a, b) => b.messages[0].createdAt - a.messages[0].createdAt
-    );
-    const mates = [];
-    for (const channel of userChannels) {
-      for (const channelMembers of channel.members) {
-        if (channelMembers.user_id !== user_id) {
-          mates.push(channelMembers);
-        }
-      }
-    }
-
-    if (mates?.length <= 0) {
-      return res.status(404).json({ error: "No friends found" });
-    }
-
-    return res.status(200).json(mates);
-  } catch (error) {
-    return res.status(500).json({ error: "Something went wrong" });
-  }
-});
-export { getUsers, updateUser, deleteUser, getUser, getUserFriends };
+export { getUsers, updateUser, deleteUser, getUser };
